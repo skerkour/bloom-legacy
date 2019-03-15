923a3e66a3732b478e5df3b1590c673ead520cdc
z0mbie42 <sylvain@kerkour.com>
2019-03-15 19:04:34 +0100

    start emails

---
+++ src/services/account/notifications/emails/account_verification_code.rs
@@ -0,0 +1,36 @@
+use crate::error::KernelError;
+
+
+fn send_account_verification_code(email: &str, first_name: &str, last_name: &str,
+pending_account_id: &str, code: &str) -> Result<(), KernelError> {
+
+    let formatted_code = code.to_string().insert(3, '-');
+	// escapedID := url.QueryEscape(id)
+	// formattedToken := token[:4] + "-" + token[4:]
+	// verifyURL := config.WWWHost + "/welcome/verify?id=" + escapedID + "&token=" + token
+
+	// from := mail.NewEmail("Bloom", HelloSenderAddress)
+	// subject := fmt.Sprintf("Confirmation code: %s", formattedToken)
+	// to := mail.NewEmail(firstName+" "+lastName, email)
+	// content := mail.NewContent(
+	// 	"text/html",
+	// 	fmt.Sprintf(`Welcome aboard! <br/>
+	// 		Your confirmation code is: <br/>
+	// 		<h2>%s</h2> <br/>
+	// 		This code will only be valid for 30 minutes. <br/>
+	// 		If you did not ask for a code, please ignore this email.
+	// 		<hr>
+	// 		You can also use the following link to verify your account: <br/>
+	// 		<a href="%s">%s</a>
+	// 	`, formattedToken, verifyURL, verifyURL),
+	// )
+	// m := mail.NewV3MailInit(from, subject, to, content)
+	// m.Personalizations[0].SetSubstitution("{{subject}}", subject)
+	// m.SetTemplateID(DefaultTemplateID)
+
+	// client := sendgrid.NewSendClient(config.SendgridAPIKey)
+	// _, err := client.Send(m)
+
+
+    return Ok(());
+}
---
+++ src/services/common/notifications/emails/mod.rs
@@ -0,0 +1,3 @@
+static HELLO_ADDRESS: &str = "hello@bloom.sh";
+static NOTIFY_ADDRESS: &str = "notify@bloom.sh";
+static SECURITY_ADDRESS: &str = "security@bloom.sh";
