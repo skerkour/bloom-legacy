eea34ff8de38add547c8b5ddbc3d127e3b6714b9
z0mbie42 <sylvain@kerkour.com>
2019-03-19 23:16:22 +0100

    account: start sessions

--- src/services/account/domain/session/mod.rs
+++ src/services/account/domain/session/mod.rs
@@ -6,12 +6,14 @@ use failure::Error;
 use crate::{
     api,
     db::DbActor,
-    db::schema::account_pending_accounts,
+    db::schema::account_sessions,
+    db::schema::account_sessions_events,
 };
 use crate::error::KernelError;
 
 
-#[derive(Clone, Debug, Deserialize, Queryable, Serialize)]
+#[derive(Clone, Debug, Deserialize, Insertable, Queryable, Serialize)]
+#[table_name = "account_sessions"]
 pub struct Session {
     pub id: uuid::Uuid,
     pub created_at: chrono::DateTime<chrono::Utc>,
@@ -19,8 +21,43 @@ pub struct Session {
     pub deleted_at: Option<chrono::DateTime<chrono::Utc>>,
     pub version: i64,
 
-    pub token: String,
+    pub device: Device,
     pub ip: String,
-    // Location  *SessionLocation
-	// Device    SessionDevice
+    pub location: Location,
+    pub token: String,
+
+    pub account_id: uuid::Uuid,
+}
+
+#[derive(AsJsonb, Clone, Debug, Deserialize, Serialize)]
+pub enum Device {}
+
+#[derive(AsJsonb, Clone, Debug, Deserialize, Serialize)]
+pub enum Location {}
+
+
+#[derive(Clone, Debug, Deserialize, Insertable, Queryable, Serialize)]
+#[table_name = "account_sessions_events"]
+pub struct Event {
+    pub id: uuid::Uuid,
+    pub timestamp: chrono::DateTime<chrono::Utc>,
+    pub data: EventData,
+    pub aggregate_id: uuid::Uuid,
+    pub metadata: EventMetadata, // TODO: change
+}
+
+#[derive(Clone, Debug)]
+pub enum Command {
+}
+
+#[derive(AsJsonb, Clone, Debug, Deserialize, Serialize)]
+pub enum EventData {
+}
+
+#[derive(AsJsonb, Clone, Debug, Deserialize, Serialize)]
+pub struct EventMetadata {
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum NonPersistedData {
 }
