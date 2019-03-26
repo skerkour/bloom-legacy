df23348a3c6fa81d50958e86098e11cfa967c879
z0mbie42 <sylvain@kerkour.com>
2019-03-26 10:47:08 +0100

    crate: improve mimesniff algo

--- crates/mimesniff-rs/src/lib.rs
+++ crates/mimesniff-rs/src/lib.rs
@@ -3,23 +3,14 @@ pub const SNIFF_LENGTH: usize = 512;
 
 /// Returns whether a buffer is JPEG image data.
 pub fn is_jpeg(buf: &[u8]) -> bool {
-    return buf.len() > 2
-        && buf[0] == 0xFF
-        && buf[1] == 0xD8
-        && buf[2] == 0xFF;
+    let pattern = [0xFF, 0xD8, 0xFF];
+    return buf.len() > 3 && buf[0..3] == pattern;
 }
 
 /// Returns whether a buffer is PNG image data.
 pub fn is_png(buf: &[u8]) -> bool {
-    return buf.len() > 7
-        && buf[0] == 0x89
-        && buf[1] == 0x50
-        && buf[2] == 0x4E
-        && buf[3] == 0x47
-        && buf[4] == 0x0D
-        && buf[5] == 0x0A
-        && buf[6] == 0x1A
-        && buf[7] == 0x0A;
+    let pattern = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
+    return buf.len() > 8 && buf[0..8] == pattern;
 }
 
 
@@ -44,9 +35,9 @@ mod tests {
     fn is_jpeg() {
         let v1: Vec<u8> = vec!(1, 2, 3);
         assert_eq!(super::is_jpeg(&v1), false);
-        let v2: Vec<u8> = vec!(0xFF, 0xD8, 0xFF);
+        let v2: Vec<u8> = vec!(0xFF, 0xD8, 0xFF, 0xff);
         assert_eq!(super::is_jpeg(&v2), true);
-        let v3: Vec<u8> = vec!(0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A);
+        let v3: Vec<u8> = vec!(0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0xff);
         assert_eq!(super::is_jpeg(&v3), false);
     }
 
@@ -54,9 +45,9 @@ mod tests {
     fn is_png() {
         let v1: Vec<u8> = vec!(1, 2, 3);
         assert_eq!(super::is_png(&v1), false);
-        let v2: Vec<u8> = vec!(0xFF, 0xD8, 0xFF);
+        let v2: Vec<u8> = vec!(0xFF, 0xD8, 0xFF, 0xff);
         assert_eq!(super::is_png(&v2), false);
-        let v3: Vec<u8> = vec!(0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A);
+        let v3: Vec<u8> = vec!(0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0xff);
         assert_eq!(super::is_png(&v3), true);
     }
 
@@ -65,9 +56,9 @@ mod tests {
     fn detect_content_type() {
         let v1: Vec<u8> = vec!(1, 2, 3);
         assert_eq!(super::detect_content_type(&v1), mime::APPLICATION_OCTET_STREAM);
-        let v2: Vec<u8> = vec!(0xFF, 0xD8, 0xFF);
+        let v2: Vec<u8> = vec!(0xFF, 0xD8, 0xFF, 0xff);
         assert_eq!(super::detect_content_type(&v2), mime::IMAGE_JPEG);
-        let v3: Vec<u8> = vec!(0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A);
+        let v3: Vec<u8> = vec!(0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0xff);
         assert_eq!(super::detect_content_type(&v3), mime::IMAGE_PNG);
     }
 }
