cd6424c8a8add42957200d0dc80abd33b56eb4ea
z0mbie42 <sylvain@kerkour.com>
2019-03-29 15:08:55 +0100

    notes: controllers

---
+++ src/services/notes/controllers/archive_note.rs
@@ -0,0 +1,73 @@
+use actix::{Message, Handler};
+use crate::{
+    db::DbActor,
+    services::account::domain::{
+        Account,
+    },
+    services::notes::domain::{
+        note,
+        Note,
+    },
+    services::common::events::EventMetadata,
+};
+use crate::error::KernelError;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ArchiveNote {
+    pub note_id: uuid::Uuid,
+    pub actor_id: uuid::Uuid,
+    pub session_id: uuid::Uuid,
+    pub request_id: uuid::Uuid,
+}
+
+impl Message for ArchiveNote {
+    type Result = Result<(), KernelError>;
+}
+
+impl Handler<ArchiveNote> for DbActor {
+    type Result = Result<(), KernelError>;
+
+    fn handle(&mut self, msg: ArchiveNote, _: &mut Self::Context) -> Self::Result {
+        use crate::db::schema::{
+            notes_notes,
+            notes_notes_events,
+        };
+        use diesel::prelude::*;
+
+
+        let conn = self.pool.get()
+            .map_err(|_| KernelError::R2d2)?;
+
+        return Ok(conn.transaction::<_, KernelError, _>(|| {
+            let metadata = EventMetadata{
+                actor_id: Some(msg.actor_id),
+                request_id: Some(msg.request_id),
+                session_id: Some(msg.session_id),
+            };
+            let archive_cmd = note::Archive{
+                metadata,
+            };
+
+            let note_to_update: Note = notes_notes::dsl::notes_notes
+                .filter(notes_notes::dsl::id.eq(msg.note_id))
+                .filter(notes_notes::dsl::owner_id.eq(msg.actor_id))
+                .filter(notes_notes::dsl::deleted_at.is_null())
+                .for_update()
+                .first(&conn)?;
+
+            let (note_to_update, event, _) = eventsourcing::execute(&conn, note_to_update, &archive_cmd)?;
+
+            // update note
+            diesel::update(&note_to_update)
+                .set(&note_to_update)
+                .execute(&conn)?;
+            diesel::insert_into(notes_notes_events::dsl::notes_notes_events)
+                .values(&event)
+                .execute(&conn)?;
+
+            return Ok(());
+        })?);
+    }
+}
--- src/services/notes/controllers/mod.rs
+++ src/services/notes/controllers/mod.rs
@@ -2,9 +2,19 @@ mod create_note;
 mod find_user_notes;
 mod find_user_trash;
 mod find_user_archive;
+mod archive_note;
+mod delete_note;
+mod remove_note;
+mod restore_note;
+mod unarchive_note;
 
 
 pub use create_note::CreateNote;
 pub use find_user_notes::FindUserNotes;
 pub use find_user_archive::FindUserArchive;
 pub use find_user_trash::FindUserTrash;
+pub use archive_note::ArchiveNote;
+pub use delete_note::DeleteNote;
+pub use remove_note::RemoveNote;
+pub use restore_note::RestoreNote;
+pub use unarchive_note::UnarchiveNote;
