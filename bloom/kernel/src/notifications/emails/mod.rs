a56b060b1a2547a62531e9f6bba83720194a3d67
z0mbie42 <sylvain@kerkour.com>
2019-04-24 18:22:10 +0200

    update lettre to fix openssl dependency problem

--- bloom/kernel/src/notifications/emails/mod.rs
+++ bloom/kernel/src/notifications/emails/mod.rs
@@ -1,9 +1,13 @@
 mod default_template;
 
 use crate::config::Config;
-use lettre::{EmailTransport, SmtpTransport};
-use lettre::smtp::authentication::{Credentials};
+use lettre::{
+    SmtpClient,
+    Transport,
+    smtp::authentication::Credentials,
+};
 use lettre_email::EmailBuilder;
+
 
 pub use default_template::DEFAULT_TEMPLATE;
 pub static HELLO_ADDRESS: &str = "hello@bloom.sh";
@@ -24,13 +28,13 @@ pub fn send_email(config: &Config, from: (&str, &str), to: (&str, &str), subject
         .build()
         .expect("error building email");
 
-    let mut mailer = SmtpTransport::simple_builder(config.smtp_host().as_str()).expect("error building emai ltransport")
+    let mut mailer = SmtpClient::new_simple(config.smtp_host().as_str()).expect("error building emai ltransport")
         // Add credentials for authentication
         .credentials(Credentials::new(config.smtp_username(), config.smtp_password()))
         // Enable SMTPUTF8 if the server supports it
         .smtp_utf8(true)
-        .build();
+        .transport();
 
     // Send the email
-    mailer.send(&email).expect("error sending email");
+    mailer.send(email.into()).expect("error sending email");
 }
