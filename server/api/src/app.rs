8b7745a15685def3cc69c22abbf2598ac0468a2c
z0mbie42 <sylvain@kerkour.com>
2019-04-27 17:20:46 +0200

    bitflow: fix clear history api

--- server/api/src/app.rs
+++ server/api/src/app.rs
@@ -175,7 +175,7 @@ pub fn init(db: actix::Addr<DbActor>, cfg: config::Config) -> App<api::State> {
             })
             .resource("/bitflow/v1/history", |r| {
                 r.method(http::Method::GET).f(bitflowv1::history::get);
-                 r.method(http::Method::GET).f(bitflowv1::history::delete);
+                 r.method(http::Method::DELETE).f(bitflowv1::history::delete);
             })
 
             .register()
