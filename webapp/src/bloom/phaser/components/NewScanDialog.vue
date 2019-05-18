ff525eb444e452d8ed4132f33acb05543fe8c632
z0mbie42 <sylvain@kerkour.com>
2019-05-18 18:29:58 +0200

    webapp/phaser: fix profiles in NewScanDialog

--- webapp/src/bloom/phaser/components/NewScanDialog.vue
+++ webapp/src/bloom/phaser/components/NewScanDialog.vue
@@ -98,7 +98,7 @@ export default class NewScanDialog extends Vue {
   // data
   error = '';
   is_loading = false;
-  profiles = [Profile.Application, Profile.Application];
+  profiles = [Profile.Network, Profile.Application];
   schedule_enum = [Schedule.Daily, Schedule.Weekly, Schedule.Monthly, Schedule.Never];
   name = '';
   description = '';
