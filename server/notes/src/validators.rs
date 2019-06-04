46838c5e08420a7375865aabcef68cf43266c455
z0mbie42 <sylvain@kerkour.fr>
2019-06-05 00:07:20 +0200

    server: improve validators

--- server/notes/src/validators.rs
+++ server/notes/src/validators.rs
@@ -2,8 +2,8 @@ use kernel::KernelError;
 
 
 pub fn title(title: &str) -> Result<(), KernelError> {
-    if title.len() > 256 {
-        return Err(KernelError::Validation("Title's length must be lower than 257 characters".to_string()));
+    if title.len() > 64 {
+        return Err(KernelError::Validation("Title length is too long".to_string()));
     }
 
     return Ok(());
