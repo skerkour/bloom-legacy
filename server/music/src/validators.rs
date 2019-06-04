09659970d105a855d0ab8942f5b4f24249965d25
z0mbie42 <sylvain@kerkour.fr>
2019-06-05 00:06:10 +0200

    server: improve validators

--- server/music/src/validators.rs
+++ server/music/src/validators.rs
@@ -1,7 +1,19 @@
 use kernel::KernelError;
 
-// TODO
-pub fn playlist_name(_playlist_name: &str) -> Result<(), KernelError> {
+pub fn playlist_name(playlist_name: &str) -> Result<(), KernelError> {
+    if playlist_name.is_empty() {
+        return Err(KernelError::Validation("playlist name cannot be empty".to_string()));
+    }
+    if playlist_name.trim() != playlist_name {
+        return Err(KernelError::Validation("playlist name is not valid".to_string()));
+    }
 
+    if playlist_name.len() < 4 {
+        return Err(KernelError::Validation("playlist name is too short".to_string()));
+    }
+
+    if playlist_name.len() > 64 {
+        return Err(KernelError::Validation("playlist name is too long".to_string()));
+    }
     return Ok(());
 }
