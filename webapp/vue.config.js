8ba92bac6bffd7bcd4ee9d4f6ca617cd455d250a
z0mbie42 <sylvain@kerkour.fr>
2019-06-08 23:18:02 +0200

    webapp: Fix favicon for Firefox

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -2,12 +2,12 @@ module.exports = {
   pwa: {
     name: 'Bloom',
     iconPaths: {
-      favicon32: '/kernel/static/imgs/logos/bloom_64.png',
-      favicon32: '/kernel/static/imgs/logos/bloom_64.png',
-      favicon16: '/kernel/static/imgs/logos/bloom_32.png',
-      appleTouchIcon: '/kernel/static/imgs/logos/bloom_256.png',
-      maskIcon: '/kernel/static/imgs/logos/bloom_256.png',
-      msTileImage: '/kernel/static/imgs/logos/bloom_256.png',
+      favicon32: 'kernel/static/imgs/logos/bloom_64.png',
+      favicon32: 'kernel/static/imgs/logos/bloom_64.png',
+      favicon16: 'kernel/static/imgs/logos/bloom_32.png',
+      appleTouchIcon: 'kernel/static/imgs/logos/bloom_256.png',
+      maskIcon: 'kernel/static/imgs/logos/bloom_256.png',
+      msTileImage: 'kernel/static/imgs/logos/bloom_256.png',
     },
   },
 };
