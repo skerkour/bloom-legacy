022851444369113b2bb7aeb05ca8329064c65276
z0mbie42 <sylvain@kerkour.fr>
2019-06-10 18:01:30 +0200

    admin/webapp: Add the /accounts route in dawer

--- webapp/src/bloom/admin/components/Drawer.vue
+++ webapp/src/bloom/admin/components/Drawer.vue
@@ -1,6 +1,15 @@
 <template>
   <v-list>
 
+        <v-list-tile exact to="/admin/accounts">
+      <v-list-tile-action>
+        <v-icon>mdi-account</v-icon>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Accounts</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
   </v-list>
 </template>
 
