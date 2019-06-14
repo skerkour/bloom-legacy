53ac4f92d4fe7e3bc78c8454814c546c72471ba7
z0mbie42 <sylvain@kerkour.fr>
2019-06-14 13:14:47 +0200

    webapp/kernel: About

--- webapp/src/bloom/kernel/views/about/Index.vue
+++ webapp/src/bloom/kernel/views/about/Index.vue
@@ -13,7 +13,7 @@
         <v-card class="elevation-0">
           <v-card-text>
             <p class="font-18 font-weight-regular">
-              We believe that a world of sharing and collaboration is the only way to build a sustainable future.
+              We believe that sharing and collaboration is the only way to build a sustainable future in a finite world.
               We create the open infrastructure to change all
               work from read-only to read-write. When everyone can contribute, users
               become contributors and we greatly improve the quality of global human life.
