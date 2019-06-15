df9eacb73d82304d4f589c986d82ebb2c5a92662
z0mbie42 <sylvain@kerkour.fr>
2019-06-15 17:49:28 +0200

    admin/webapp: Search by email or username

--- webapp/src/bloom/admin/components/Accounts.vue
+++ webapp/src/bloom/admin/components/Accounts.vue
@@ -1,44 +1,71 @@
 <template>
-  <div>
-    <v-alert icon="mdi-alert-circle" :value="error" type="error" dismissible>
-      {{ error }}
-    </v-alert>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-alert icon="mdi-alert-circle" :value="error" type="error" dismissible>
+          {{ error }}
+        </v-alert>
+      </v-flex>
 
-    <v-data-table
-      :headers="headers"
-      :items="accounts"
-      hide-actions
-      :loading="is_loading"
-      :pagination.sync="pagination"
-      :total-items="total">
-      <template slot="no-data">
-        <p class="text-xs-center">
-          No accounts.
-        </p>
-      </template>
-      <template v-slot:items="props">
-        <td class="text-xs-left">
-          <span>{{ props.item.username }}</span>
-        </td>
-        <td class="text-xs-left">
-          <v-chip color="success" outline v-if="!props.item.is_disabled">Active</v-chip>
-          <v-chip v-else outline>Disabled</v-chip>
-        </td>
-        <td class="justify-left">
-          <v-tooltip bottom>
-            <v-btn flat icon small color="grey darken-1" slot="activator"
-              :to="`/admin/accounts/${props.item.id}`">
-              <v-icon small>mdi-magnify</v-icon>
-            </v-btn>
-            <span>View account</span>
-          </v-tooltip>
-        </td>
-      </template>
-    </v-data-table>
-    <div class="text-xs-center pt-2">
-      <v-pagination v-model="pagination.page" :length="pages" :total-visible="8" @input="paginate"></v-pagination>
-    </div>
-  </div>
+      <v-flex xs12 sm5>
+        <v-text-field
+          label="Username"
+          v-model="username"
+          clearable
+        ></v-text-field>
+      </v-flex>
+
+      <v-flex xs12 sm5>
+        <v-text-field
+          label="Email"
+          v-model="email"
+          clearable
+        ></v-text-field>
+      </v-flex>
+
+      <v-flex xs12 sm2>
+        <v-btn @click="fetch_data">
+          Seach
+        </v-btn>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-data-table
+          :headers="headers"
+          :items="accounts"
+          hide-actions
+          :loading="is_loading"
+          :pagination.sync="pagination"
+          :total-items="total">
+          <template slot="no-data">
+            <p class="text-xs-center">
+              No accounts.
+            </p>
+          </template>
+          <template v-slot:items="props">
+            <td class="text-xs-left">
+              <span>{{ props.item.username }}</span>
+            </td>
+            <td class="text-xs-left">
+              <v-chip color="success" outline v-if="!props.item.is_disabled">Active</v-chip>
+              <v-chip v-else outline>Disabled</v-chip>
+            </td>
+            <td class="justify-left">
+              <v-tooltip bottom>
+                <v-btn flat icon small color="grey darken-1" slot="activator"
+                  :to="`/admin/accounts/${props.item.id}`">
+                  <v-icon small>mdi-magnify</v-icon>
+                </v-btn>
+                <span>View account</span>
+              </v-tooltip>
+            </td>
+          </template>
+        </v-data-table>
+        <div class="text-xs-center pt-2">
+          <v-pagination v-model="pagination.page" :length="pages" :total-visible="8" @input="paginate"></v-pagination>
+        </div>
+      </v-flex>
+
+    </v-layout>
 </template>
 
 
@@ -56,6 +83,8 @@ export default class Accounts extends Vue {
   // data
   error = '';
   is_loading = false;
+  username = '';
+  email = '';
   offset = 0;
   total = 0;
   accounts: any[] = [];
@@ -117,8 +146,10 @@ export default class Accounts extends Vue {
     try {
       const res = await api.get(`${api.ADMIN}/v1/accounts`, {
         params: {
+          email: this.email,
           limit: LIMIT,
           offset,
+          username: this.username,
         },
       });
       this.accounts = res.hits;
