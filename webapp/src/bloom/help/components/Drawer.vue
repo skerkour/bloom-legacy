87780d5b3450c094646236571caaf10659ab7974
z0mbie42 <sylvain@kerkour.fr>
2019-06-18 15:15:43 +0200

    help/webapp: Drawer

--- webapp/src/bloom/help/components/Drawer.vue
+++ webapp/src/bloom/help/components/Drawer.vue
@@ -2,14 +2,33 @@
   <div>
     <v-list>
 
-      <v-list-tile exact to="/help">
-        <v-list-tile-action>
-          <v-icon color="grey darken-1">mdi-home</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>Home</v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
+    <v-list-tile exact to="/help">
+      <v-list-tile-action>
+        <v-icon color="grey darken-1">mdi-home</v-icon>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Home</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
+    <v-list-tile exact to="/help/drive">
+      <v-list-tile-action>
+        <v-avatar :size="32"><img src="/kernel/static/imgs/logos/drive_64.png" /></v-avatar>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Drive</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
+
+    <v-list-tile exact to="/help/platform">
+      <v-list-tile-action>
+        <v-avatar :size="32"><img src="/kernel/static/imgs/logos/platform_64.png" /></v-avatar>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Platform</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
 
     <v-list-tile exact to="/help/phaser">
       <v-list-tile-action>
@@ -19,6 +38,79 @@
         <v-list-tile-title>Phaser</v-list-tile-title>
       </v-list-tile-content>
     </v-list-tile>
+
+
+    <v-list-tile exact to="/help/myaccount">
+      <v-list-tile-action>
+        <v-avatar :size="32"><img src="/kernel/static/imgs/logos/myaccount_64.png" /></v-avatar>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>MyAccount</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
+
+    <v-list-tile exact to="/help/bitflow">
+      <v-list-tile-action>
+        <v-avatar :size="32"><img src="/kernel/static/imgs/logos/bitflow_64.png" /></v-avatar>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Bitflow</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
+
+    <v-list-tile exact to="/help/music">
+      <v-list-tile-action>
+        <v-avatar :size="32"><img src="/kernel/static/imgs/logos/music_64.png" /></v-avatar>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Music</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
+
+    <v-list-tile exact to="/help/gallery">
+      <v-list-tile-action>
+        <v-avatar :size="32"><img src="/kernel/static/imgs/logos/gallery_64.png" /></v-avatar>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Gallery</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
+
+
+    <v-list-tile exact to="/help/contacts">
+      <v-list-tile-action>
+        <v-avatar :size="32"><img src="/kernel/static/imgs/logos/contacts_64.png" /></v-avatar>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Contacts</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
+
+    <v-list-tile exact to="/help/arcade">
+      <v-list-tile-action>
+        <v-avatar :size="32"><img src="/kernel/static/imgs/logos/arcade_64.png" /></v-avatar>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Arcade</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
+
+    <v-list-tile exact to="/help/calendar">
+      <v-list-tile-action>
+        <v-avatar :size="32"><img src="/kernel/static/imgs/logos/calendar_64.png" /></v-avatar>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>Calendar</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
+
 
   </v-list>
   </div>
