b09d13d31889de63bcef0026506539ceeffcf026
z0mbie42 <sylvain@kerkour.fr>
2019-06-18 15:43:42 +0200

    help/webapp: Add Notes in drawer

--- webapp/src/bloom/help/views/Index.vue
+++ webapp/src/bloom/help/views/Index.vue
@@ -2,24 +2,29 @@
   <v-container >
     <v-layout row wrap>
 
-      <v-flex xs12>
-        <h1>Welcome to Bloom's help and documentation center</h1>
+      <v-flex xs12 class="mb-5 mt-3">
+        <h1 class="display-1 font-weight-medium">
+          Welcome to Bloom's help and documentation center
+        </h1>
       </v-flex>
 
       <v-flex xs12>
-        <v-container fluid grid-list-sm>
-          <v-layout row wrap justify-left>
-            <v-flex v-for="card in cards" xs4 sm3 lg2 :key="card.title">
-              <v-tooltip bottom>
-                <router-link :to="card.to" slot="activator">
-                  <v-img :src="card.src" height="100px" contain />
-                  <p class="headline text-xs-center app-name mt-1">{{ card.name }}</p>
-                </router-link>
-                <span>{{ card.tooltip }}</span>
-              </v-tooltip>
-            </v-flex>
-          </v-layout>
-        </v-container>
+        <v-layout row wrap justify-left>
+          <v-flex v-for="card in cards" xs4 sm3 lg2 :key="card.title">
+            <v-tooltip bottom>
+              <router-link :to="card.to" slot="activator">
+                <v-img :src="card.src" height="100px" contain />
+                <p class="headline text-xs-center app-name mt-1">{{ card.name }}</p>
+              </router-link>
+              <span>{{ card.tooltip }}</span>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+
+
+      <v-flex xs12 class="mt-5 font-18">
+        <p>If you need further help, please visit our <router-link to="/contact">Contact page</router-link>.</p>
       </v-flex>
 
     </v-layout>
@@ -115,6 +120,10 @@ export default class Index extends Vue {
 <style scoped lang="scss">
 a {
   text-decoration: none;
+}
+
+.font-18 {
+  font-size: 18px;
 }
 
 .app-name {
