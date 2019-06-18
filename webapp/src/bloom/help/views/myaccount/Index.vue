ba642b091bec354968db1f474f44d0b4998e0da5
z0mbie42 <sylvain@kerkour.fr>
2019-06-18 15:52:16 +0200

    help/webapp: Add Dark mode help for myaccount

--- webapp/src/bloom/help/views/myaccount/Index.vue
+++ webapp/src/bloom/help/views/myaccount/Index.vue
@@ -3,7 +3,22 @@
     <v-layout row wrap>
 
       <v-flex xs12>
-        <h1>MyAccount</h1>
+        <h1 class="display-1 font-weight-medium">
+          MyAccount
+        </h1>
+      </v-flex>
+
+
+      <v-flex xs12 class="mt-4">
+        <h2 class="headline font-weight-medium">
+          Dark theme
+        </h2>
+
+        <p class="font-18">
+          The switch to activate / deactivate the dark theme can be found at the bottom of your profile page, in
+          the <router-link to="/myaccount">MyAccount app</router-link>.
+        </p>
+        <v-img max-height="600px" contain src="/help/static/myaccount/dark_mode.png" />
       </v-flex>
 
     </v-layout>
@@ -27,4 +42,11 @@ export default class Index extends Vue {
 
 
 <style scoped lang="scss">
+a {
+  text-decoration: none;
+}
+
+.font-18 {
+  font-size: 18px;
+}
 </style>
