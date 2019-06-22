19fd2e9aa5aab47b2feced2653642168aadfc8d5
z0mbie42 <sylvain@kerkour.fr>
2019-06-23 01:17:17 +0200

    calendar/webapp: Create Event dialog

--- webapp/src/bloom/calendar/views/Index.vue
+++ webapp/src/bloom/calendar/views/Index.vue
@@ -1,29 +1,26 @@
 <template>
 <div class="fill-height">
-  <v-layout row wrap class="blm-calendar-controls mb-2 mt-2">
-    <v-flex xs4 class="text-xs-right pr-4">
-      <v-btn outline color="primary" @click="$refs.calendar.prev()">
-        <v-icon dark left>
-          mdi-chevron-left
-        </v-icon>
-        Prev
+  <v-layout row wrap class="blm-calendar-controls mt-2" align-center>
+
+    <v-flex xs12 d-inline-block class="pr-4">
+      <v-btn outline fab small color="primary" @click="$refs.calendar.prev()" class="mr-2">
+        <v-icon dark>mdi-chevron-left</v-icon>
       </v-btn>
-    </v-flex>
-    <v-flex xs4 class="text-xs-center">
       <v-select
         v-model="type"
         :items="type_options"
         label="Type"
+        dense
+        :full-width="false"
       ></v-select>
-    </v-flex>
-    <v-flex xs4 class="text-xs-left pl-4">
-      <v-btn outline color="primary" @click="$refs.calendar.next()">
-        Next
-        <v-icon right dark>
-          mdi-chevron-right
-        </v-icon>
+      <v-btn outline fab small right color="primary" @click="$refs.calendar.next()" class="ml-4">
+        <v-icon dark>mdi-chevron-right</v-icon>
+      </v-btn>
+      <v-btn outline right absolute color="primary" class="hidden-sm-and-down" @click="open_dialog">
+        <v-icon left>mdi-plus</v-icon>Add
       </v-btn>
     </v-flex>
+
   </v-layout>
 
   <v-layout wrap class="blm-calendar">
@@ -38,6 +35,20 @@
       ></v-calendar>
     </v-flex>
   </v-layout>
+
+
+    <blm-calendar-dialog-event
+      :event="current_event"
+      :visible="event_dialog"
+      @close="close_event_dialog"
+      @create="event_created"
+      @update="event_updated"
+      @delete="event_deleted"
+    />
+
+  <v-btn @click="open_dialog" color="red" dark fab fixed bottom right class="hidden-md-and-up">
+    <v-icon>mdi-plus</v-icon>
+  </v-btn>
 </div>
 </template>
 
@@ -45,13 +56,21 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import moment from 'moment';
+import EventDialog from '../components/EventDialog.vue';
 
 
-@Component
+@Component({
+  components: {
+    'blm-calendar-dialog-event': EventDialog,
+  },
+})
 export default class Calendar extends Vue {
   // props
   // data
   type = 'month';
+  event_dialog = false;
+  current_event: any | null = null;
+  events: any[] = [];
   current_day = moment().format('YYYY-MM-DD');
   end = moment().endOf('month').format('YYYY-MM-DD');
   type_options = [
@@ -60,19 +79,47 @@ export default class Calendar extends Vue {
     { text: 'Month', value: 'month' },
   ];
   weekdays = [1, 2, 3, 4, 5, 6, 0];
+
+
   // computed
   // lifecycle
   // watch
   // methods
+    open_dialog() {
+    this.event_dialog = true;
+  }
+
+  close_event_dialog() {
+    this.event_dialog = false;
+    this.current_event = null;
+  }
+
+  event_created(event: any) {
+    this.events.push(event);
+  }
+
+  event_updated(updated_event: any) {
+    this.events = this.events.map((event: any) => {
+      return event.id === updated_event.id ? updated_event : event;
+    });
+  }
+
+  event_deleted(deleted_event: any) {
+    this.events = this.events.filter((c) => c.id !== deleted_event.id);
+  }
 }
 </script>
 
 
 <style scoped lang="scss">
 .blm-calendar {
-  height: calc(100% - 60px);
+  height: calc(100% - 76px);
 }
 .blm-calendar-controls {
-  height: 60px;
+  height: 68px;
+}
+
+.v-select {
+  display: inline-block !important;
 }
 </style>
