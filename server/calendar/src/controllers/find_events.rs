c56c1d33567cff500e6b0972aaadd26cb324e774
z0mbie42 <sylvain@kerkour.fr>
2019-06-23 15:00:05 +0200

    calendar/server: Fix find_events for user

--- server/calendar/src/controllers/find_events.rs
+++ server/calendar/src/controllers/find_events.rs
@@ -4,10 +4,6 @@ use kernel::{
     db::DbActor,
     KernelError,
 };
-use diesel::{
-    sql_query,
-    pg::types::sql_types,
-};
 use crate::{
     domain,
 };
@@ -28,14 +24,18 @@ impl Handler<FindEvents> for DbActor {
     type Result = Result<Vec<domain::CalendarEvent>, KernelError>;
 
     fn handle(&mut self, msg: FindEvents, _: &mut Self::Context) -> Self::Result {
+        use kernel::db::schema::{
+            calendar_events,
+        };
         use diesel::prelude::*;
 
         let conn = self.pool.get()
             .map_err(|_| KernelError::R2d2)?;
 
-        let cal_events: Vec<domain::CalendarEvent> = sql_query(include_str!("../../sql_requests/find_events.sql"))
-            .bind::<sql_types::Timestamptz, _>(msg.start_at)
-            .bind::<sql_types::Timestamptz, _>(msg.end_at)
+        let cal_events: Vec<domain::CalendarEvent> = calendar_events::dsl::calendar_events
+            .filter(calendar_events::dsl::owner_id.eq(msg.owner_id))
+            .filter(calendar_events::dsl::deleted_at.is_null())
+            .filter(calendar_events::dsl::start_at.between(msg.start_at, msg.end_at))
             .load(&conn)?;
 
         return Ok(cal_events);
