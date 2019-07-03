a422cdff79b042a7a91fe6df7170174faa40cc55
z0mbie42 <sylvain@kerkour.fr>
2019-07-03 10:53:37 +0200

    bitflow/server: fix magnet url parsing

--- server/bitflow/src/validators.rs
+++ server/bitflow/src/validators.rs
@@ -13,34 +13,6 @@ pub fn download_url(download_url: &str) -> Result<(), KernelError> {
         ));
     }
 
-    match parsed_url.host() {
-        Some(host) => {
-            let invalid_hosts = vec![
-                "127.0.0.1",
-                "localhost",
-                "0.0.0.0",
-                "::1:",
-                "bloom.sh",
-                "::1",
-                "[::1]",
-                "169.254.169.254",          // aws / DO metadata endpoint
-                "metadata.google.internal", // Google cloud platform metadata endpoint
-                "metadata.packet.net",      // packet.netmetadata endpoint
-            ];
-            if invalid_hosts.contains(&host.to_string().as_str()) {
-                return Err(KernelError::Validation("host is not valid".to_string()));
-            }
-            if let Ok(ip_address) = host.to_string().parse::<net::IpAddr>() {
-                if !ip_address.is_global() {
-                    return Err(KernelError::Validation(
-                        "private IP addresses are not valid".to_string(),
-                    ));
-                }
-            }
-        }
-        None => return Err(KernelError::Validation("Url is not valid.".to_string())),
-    }
-
     match scheme {
         "magnet" => {
             let re = Regex::new(r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*")
@@ -51,7 +23,35 @@ pub fn download_url(download_url: &str) -> Result<(), KernelError> {
                 ));
             }
         }
-        _ => {}
+        _ => {
+            match parsed_url.host() {
+                Some(host) => {
+                    let invalid_hosts = vec![
+                        "127.0.0.1",
+                        "localhost",
+                        "0.0.0.0",
+                        "::1:",
+                        "bloom.sh",
+                        "::1",
+                        "[::1]",
+                        "169.254.169.254",          // aws / DO metadata endpoint
+                        "metadata.google.internal", // Google cloud platform metadata endpoint
+                        "metadata.packet.net",      // packet.netmetadata endpoint
+                    ];
+                    if invalid_hosts.contains(&host.to_string().as_str()) {
+                        return Err(KernelError::Validation("host is not valid".to_string()));
+                    }
+                    if let Ok(ip_address) = host.to_string().parse::<net::IpAddr>() {
+                        if !ip_address.is_global() {
+                            return Err(KernelError::Validation(
+                                "private IP addresses are not valid".to_string(),
+                            ));
+                        }
+                    }
+                }
+                None => return Err(KernelError::Validation("Url is not valid.".to_string())),
+            }
+        }
     }
 
     return Ok(());
