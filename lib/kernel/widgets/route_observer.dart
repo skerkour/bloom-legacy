c70523e8ecb98c5bf4e9898e70b3c812d7906ce6
z0mbie42 <sylvain@kerkour.fr>
2019-07-14 01:14:30 +0200

    kernel: Improve route observer

--- lib/kernel/widgets/route_observer.dart
+++ lib/kernel/widgets/route_observer.dart
@@ -8,30 +8,34 @@ class BlmRouteObserver extends RouteObserver<PageRoute<dynamic>> {
   void didPush(Route<dynamic> route, Route<dynamic> previousRoute) {
     super.didPush(route, previousRoute);
     debugPrint('didsPush');
-    _onRouteChanged(route);
+    updateDrawer(route);
   }
 
   @override
   void didReplace({Route<dynamic> newRoute, Route<dynamic> oldRoute}) {
     super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
     debugPrint('didReplace');
-    _onRouteChanged(newRoute);
+    updateDrawer(newRoute);
   }
 
   @override
   void didPop(Route<dynamic> route, Route<dynamic> previousRoute) {
     super.didPop(route, previousRoute);
     debugPrint('didPop');
-    _onRouteChanged(previousRoute);
+    updateDrawer(previousRoute);
   }
 
-  void _onRouteChanged(Route<dynamic> route) {
+  void updateDrawer(Route<dynamic> route) {
     final String routeStr = route.settings.name;
     if (routeStr == '/') {
+      debugPrint('(RouteObserver) Home');
       drawerBloc.setApp(Apps.HOME);
     } else if (routeStr.startsWith('/notes')) {
+      debugPrint('(RouteObserver) Notes');
       drawerBloc.setApp(Apps.NOTES);
     } else if (routeStr.startsWith('/contacts')) {
+      drawerBloc.setApp(Apps.CONTACTS);
+      debugPrint('(RouteObserver) Contacts');
     } else {
       debugPrint('(RouteObserver) route not found');
     }
