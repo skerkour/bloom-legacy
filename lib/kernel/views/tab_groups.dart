e1c885d7c4e9c5817708a98c2e0fc055796b32a6
z0mbie42 <sylvain@kerkour.fr>
2019-08-03 23:27:45 +0200

    kernel: Groups tab

--- lib/kernel/views/tab_groups.dart
+++ lib/kernel/views/tab_groups.dart
@@ -8,8 +8,30 @@ class TabGroupsView extends StatefulWidget {
 }
 
 class _TabGroupsViewState extends State<TabGroupsView> {
+  static const List<_Group> _groups = <_Group>[
+    _Group(name: 'Bloom', description: 'Empowering people'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text('Groups'));
+    return ListView.builder(
+      itemCount: _groups.length,
+      itemBuilder: (BuildContext context, int index) {
+        final _Group group = _groups[index];
+        return ListTile(
+          leading: CircleAvatar(child: Text(group.name[0].toUpperCase())),
+          title: Text(group.name),
+          subtitle: Text(group.description),
+          isThreeLine: false,
+        );
+      },
+    );
   }
+}
+
+class _Group {
+  const _Group({@required this.name, @required this.description, this.icon});
+  final String icon;
+  final String name;
+  final String description;
 }
