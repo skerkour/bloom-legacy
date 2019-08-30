ead30624e48502abeb912f2d2463a2de03061d05
z0mbie42 <sylvain@kerkour.fr>
2019-08-30 13:46:19 +0200

    split mobile/desktop codebases

---
+++ desktop/src/app.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow } = require('electron');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow = null;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    title: 'Bloom',
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('http://localhost:8080');
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  // if (process.platform !== 'darwin') {
+  //   app.quit();
+  // }
+  app.quit();
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ desktop/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <div class="text-center">
+      <v-btn>Button</v-btn>
+    </div>
+  </div>
+</template>
