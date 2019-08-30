921dda64171b5c0dd2020005b97ba9efaea674e8
z0mbie42 <sylvain@kerkour.fr>
2019-08-30 14:21:12 +0200

    eslint + editorconfig

--- desktop/.eslintrc.js
+++ desktop/.eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'lines-between-class-members': 'off',
+    'class-methods-use-this': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
