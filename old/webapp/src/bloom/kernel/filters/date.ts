33df1a091bd2a6f392b9d257d329f672b4b34b0c
z0mbie42 <sylvain@kerkour.fr>
2019-09-12 15:35:39 +0200

    remove old

--- old/webapp/src/bloom/kernel/filters/date.ts
+++
@@ -1,8 +0,0 @@
-import moment from 'moment';
-
-export default function(text: string, format: string) {
-  if (format === undefined) {
-    format = 'MMMM Do YYYY, HH:mm:ss';
-  }
-  return moment(text).format(format);
-}
--- old/webapp/src/bloom/kernel/filters/filesize.ts
+++
@@ -1,5 +0,0 @@
-import filesize from '@/libs/filesize';
-
-export default function(value: number) {
-  return filesize(value, { base: 10 });
-}
