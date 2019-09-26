c14264cc1a76b1c1b42836827b0f713542f78edc
z0mbie42 <sylvain@kerkour.fr>
2019-09-26 11:44:23 +0200

    migrations: legacy

--- migrations/2019-09-26-093045_legacy/down.sql
+++ migrations/2019-09-26-093045_legacy/down.sql
@@ -1,1 +1,20 @@
 -- This file should undo anything in `up.sql`
+
+ALTER TABLE bitflow_downloads_legacy RENAME TO bitflow_downloads;
+ALTER TABLE bitflow_profiles_legacy RENAME TO bitflow_profiles;
+ALTER TABLE calendar_events_legacy RENAME TO calendar_events;
+ALTER TABLE contacts_contacts_legacy RENAME TO contacts_contacts;
+ALTER TABLE drive_files_legacy RENAME TO drive_files;
+ALTER TABLE drive_profiles_legacy RENAME TO drive_profiles;
+ALTER TABLE drive_uploads_legacy RENAME TO drive_uploads;
+ALTER TABLE gallery_albums_legacy RENAME TO gallery_albums;
+ALTER TABLE gallery_albums_files_legacy RENAME TO gallery_albums_files;
+ALTER TABLE kernel_accounts_legacy RENAME TO kernel_accounts;
+ALTER TABLE kernel_pending_accounts_legacy RENAME TO kernel_pending_accounts;
+ALTER TABLE kernel_pending_emails_legacy RENAME TO kernel_pending_emails;
+ALTER TABLE kernel_sessions_legacy RENAME TO kernel_sessions;
+ALTER TABLE music_playlists_legacy RENAME TO music_playlists;
+ALTER TABLE music_playlists_files_legacy RENAME TO music_playlists_files;
+ALTER TABLE notes_notes_legacy RENAME TO notes_notes;
+ALTER TABLE phaser_reports_legacy RENAME TO phaser_reports;
+ALTER TABLE phaser_scans_legacy RENAME TO phaser_scans;
--- migrations/2019-09-26-093045_legacy/up.sql
+++ migrations/2019-09-26-093045_legacy/up.sql
@@ -1,1 +1,20 @@
 -- Your SQL goes here
+
+ALTER TABLE bitflow_downloads RENAME TO bitflow_downloads_legacy;
+ALTER TABLE bitflow_profiles RENAME TO bitflow_profiles_legacy;
+ALTER TABLE calendar_events RENAME TO calendar_events_legacy;
+ALTER TABLE contacts_contacts RENAME TO contacts_contacts_legacy;
+ALTER TABLE drive_files RENAME TO drive_files_legacy;
+ALTER TABLE drive_profiles RENAME TO drive_profiles_legacy;
+ALTER TABLE drive_uploads RENAME TO drive_uploads_legacy;
+ALTER TABLE gallery_albums RENAME TO gallery_albums_legacy;
+ALTER TABLE gallery_albums_files RENAME TO gallery_albums_files_legacy;
+ALTER TABLE kernel_accounts RENAME TO kernel_accounts_legacy;
+ALTER TABLE kernel_pending_accounts RENAME TO kernel_pending_accounts_legacy;
+ALTER TABLE kernel_pending_emails RENAME TO kernel_pending_emails_legacy;
+ALTER TABLE kernel_sessions RENAME TO kernel_sessions_legacy;
+ALTER TABLE music_playlists RENAME TO music_playlists_legacy;
+ALTER TABLE music_playlists_files RENAME TO music_playlists_files_legacy;
+ALTER TABLE notes_notes RENAME TO notes_notes_legacy;
+ALTER TABLE phaser_reports RENAME TO phaser_reports_legacy;
+ALTER TABLE phaser_scans RENAME TO phaser_scans_legacy;
