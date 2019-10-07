afe2f4b84ffa82483b8da18ddc07474efbb196f1
z0mbie42 <sylvain@kerkour.fr>
2019-10-08 00:21:50 +0200

    const: Add email addresses

--- core/bloom/const/src/lib.rs
+++ core/bloom/const/src/lib.rs
@@ -1,1 +1,6 @@
 pub mod myaccount;
+
+pub static ADDRESS_HELLO: &str = "hello@bloom.sh";
+pub static ADDRESS_WELCOME: &str = "welcome@bloom.sh";
+pub static ADDRESS_NOTIFY: &str = "notify@bloom.sh";
+pub static ADDRESS_SECURITY: &str = "security@bloom.sh";
