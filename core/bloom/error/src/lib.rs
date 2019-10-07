5bcd809a68e935c021a69d52e597bffe964ef9f6
z0mbie42 <sylvain@kerkour.fr>
2019-10-07 23:16:13 +0200

    error: Add from

--- core/bloom/error/src/lib.rs
+++ core/bloom/error/src/lib.rs
@@ -1,5 +1,8 @@
 use failure::Fail;
 
+#[cfg(feature = "io")]
+use std::io;
+
 mod code;
 
 pub use code::ErrorCode;
@@ -14,9 +17,68 @@ pub enum BloomError {
 
     #[fail(display = "Unknown message type")]
     UnknownMessageType,
+
+    #[fail(display = "ActixMailboxError")]
+    ActixMailbox,
+
+    #[fail(display = "TokioTimerError: {}", 0)]
+    TokioTimer(String),
+
+    #[fail(display = "DieselError: {}", 0)]
+    Diesel(String),
+
+    #[fail(display = "R2d2Error: {}", 0)]
+    R2d2(String),
+
+    #[fail(display = "Not valid: {}", 0)]
+    Validation(String),
+
+    #[fail(display = "Unauthorized: {}", 0)]
+    Unauthorized(String),
+
+    #[fail(display = "Forbidden: {}", 0)]
+    Forbidden(String),
+
+    #[fail(display = "Io: {}", 0)]
+    Io(String),
+
+    #[fail(display = "Image: {}", 0)]
+    Image(String),
+
+    #[fail(display = "NotFound: {}", 0)]
+    NotFound(String),
+
+    #[fail(display = "Route not found")]
+    RouteNotFound,
+
+    #[fail(display = "Timeout")]
+    Timeout,
+
+    #[fail(display = "Internal error")]
+    Internal(String),
+
+    #[fail(display = "URL parse error: {}", 0)]
+    UrlParseError(String),
+
+    #[fail(display = "Zip: {:?}", 0)]
+    Zip(String),
+
+    #[fail(display = "Walkdir: {:?}", 0)]
+    Walkdir(String),
+
+    #[fail(display = "SerdeJson: {:?}", 0)]
+    SerdeJson(String),
+
+    #[fail(display = "LettreEmail: {:?}", 0)]
+    LettreEmail(String),
+
+    #[fail(display = "LettreSmtp: {:?}", 0)]
+    LettreSmtp(String),
 }
 
 // TODO: improve...
+// TODO(z0mbie42):
+// * into message
 impl From<BloomError> for bloom_messages::kernel::Error {
     fn from(err: BloomError) -> Self {
         let code = match &err {
@@ -35,18 +97,96 @@ impl From<BloomError> for bloom_messages::kernel::Error {
     }
 }
 
+#[cfg(feature = "reqwest")]
 impl From<reqwest::Error> for BloomError {
     fn from(err: reqwest::Error) -> Self {
         BloomError::Reqwest(err)
     }
 }
 
+#[cfg(feature = "crypto42")]
 impl From<crypto42::Error> for BloomError {
     fn from(err: crypto42::Error) -> Self {
         BloomError::Crypto42(err)
     }
 }
 
-// TODO(z0mbie42):
-// * from other error types (crypto42, reqwest...)
-// * into message
+#[cfg(feature = "actix")]
+impl From<actix::MailboxError> for BloomError {
+    fn from(_e: actix::MailboxError) -> Self {
+        BloomError::ActixMailbox
+    }
+}
+
+#[cfg(feature = "diesel")]
+impl From<diesel::result::Error> for BloomError {
+    fn from(err: diesel::result::Error) -> Self {
+        match err {
+            e @ diesel::result::Error::NotFound => BloomError::NotFound(format!("{}", e)),
+            e => BloomError::Diesel(format!("{:?}", e)),
+        }
+    }
+}
+
+#[cfg(feature = "image")]
+impl From<image::ImageError> for BloomError {
+    fn from(err: image::ImageError) -> Self {
+        BloomError::Image(format!("{:?}", err))
+    }
+}
+
+#[cfg(feature = "diesel")]
+impl From<diesel::r2d2::Error> for BloomError {
+    fn from(err: diesel::r2d2::Error) -> Self {
+        BloomError::R2d2(format!("{:?}", err))
+    }
+}
+
+#[cfg(feature = "url")]
+impl From<url::ParseError> for BloomError {
+    fn from(err: url::ParseError) -> Self {
+        BloomError::UrlParseError(err)
+    }
+}
+
+#[cfg(feature = "std")]
+impl From<io::Error> for BloomError {
+    fn from(err: io::Error) -> Self {
+        BloomError::Io(format!("{:?}", err))
+    }
+}
+
+#[cfg(feature = "zip")]
+impl From<zip::result::ZipError> for BloomError {
+    fn from(err: zip::result::ZipError) -> Self {
+        BloomError::Zip(format!("{:?}", err))
+    }
+}
+
+#[cfg(feature = "walkdir")]
+impl From<walkdir::Error> for BloomError {
+    fn from(err: walkdir::Error) -> Self {
+        BloomError::Walkdir(format!("{:?}", err))
+    }
+}
+
+#[cfg(feature = "serde_json")]
+impl From<serde_json::Error> for BloomError {
+    fn from(err: serde_json::Error) -> Self {
+        BloomError::SerdeJson(format!("{:?}", err))
+    }
+}
+
+#[cfg(feature = "lettre_email")]
+impl From<lettre_email::error::Error> for BloomError {
+    fn from(err: lettre_email::error::Error) -> Self {
+        BloomError::LettreEmail(format!("{:?}", err))
+    }
+}
+
+#[cfg(feature = "lettre")]
+impl From<lettre::smtp::error::Error> for BloomError {
+    fn from(err: lettre::smtp::error::Error) -> Self {
+        BloomError::LettreSmtp(format!("{:?}", err))
+    }
+}
