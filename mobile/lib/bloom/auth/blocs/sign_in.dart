b4f3b747f85a72b92670948ef1a7dc1ddd856e5a
z0mbie42 <sylvain@kerkour.fr>
2019-10-24 21:43:03 +0200

    mobile/auth: use bloc for SignIn

--- mobile/lib/bloom/auth/blocs/sign_in.dart
+++ mobile/lib/bloom/auth/blocs/sign_in.dart
@@ -1,1 +1,49 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:bloom/bloom/calendar/models/event.dart';
+import 'package:bloom/bloom/kernel/blocs/bloc_provider.dart';
+import 'package:bloom/native/core_ffi.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
+import '../messages.dart';
+
+class SignInBloc extends BlocBase {
+  SignInBloc();
+
+  final StreamController<bool> _isLoadingController =
+      StreamController<bool>.broadcast();
+  StreamSink<bool> get _isLoadingStream => _isLoadingController.sink;
+  Stream<bool> get isLoadingStream => _isLoadingController.stream;
+
+  @override
+  void dispose() {
+    _isLoadingController.close();
+  }
+
+  Future<Map<String, dynamic>> signIn(String username, String password) async {
+    debugPrint('SignInBloc.signIn called');
+    _isLoadingController.add(true);
+
+    final String message = jsonEncode(AuthGuiSignIn(
+      username: username,
+      password: password,
+    ));
+
+    Map<String, dynamic> json;
+
+    try {
+      json = await compute(SignInBloc._nativeCall, message);
+    } catch (err) {
+      rethrow;
+    } finally {
+      _isLoadingStream.add(false);
+    }
+
+    return json;
+  }
+
+  static Map<String, dynamic> _nativeCall(String message) {
+    return coreFfi.call(message);
+  }
+}
--- mobile/lib/bloom/auth/widgets/sign_in.dart
+++ mobile/lib/bloom/auth/widgets/sign_in.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
+import 'package:bloom/bloom/auth/blocs/sign_in.dart';
 import 'package:bloom/bloom/kernel/widgets/password_field.dart';
-import 'package:bloom/native/core_ffi.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import '../messages.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key key}) : super(key: key);
@@ -14,54 +13,76 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   TextStyle style = const TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-  bool isLoading = false;
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  SignInBloc _bloc;
+
+  @override
+  void initState() {
+    _bloc = SignInBloc();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(36.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // SizedBox(
-            //   height: 155.0,
-            //   child: Image.asset(
-            //     "assets/logo.png",
-            //     fit: BoxFit.contain,
-            //   ),
-            // ),
-            // SizedBox(height: 45.0),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Username'),
-              controller: usernameController,
+    return StreamBuilder<bool>(
+        initialData: false,
+        stream: _bloc.isLoadingStream,
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          bool isLoading = false;
+          if (snapshot.hasData) {
+            isLoading = snapshot.data;
+          }
+          return Container(
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // SizedBox(
+                  //   height: 155.0,
+                  //   child: Image.asset(
+                  //     "assets/logo.png",
+                  //     fit: BoxFit.contain,
+                  //   ),
+                  // ),
+                  // SizedBox(height: 45.0),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'Username'),
+                    controller: usernameController,
+                  ),
+                  const SizedBox(height: 25.0),
+                  PasswordField(
+                    labelText: 'Password',
+                    controller: passwordController,
+                  ),
+                  const SizedBox(
+                    height: 35.0,
+                  ),
+                  _buildSignInButton(context, isLoading),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  isLoading
+                      ? const CircularProgressIndicator()
+                      : Container(
+                          width: 0,
+                          height: 0), // TODO(z0mbie42): remove ugly hack
+                ],
+              ),
             ),
-            const SizedBox(height: 25.0),
-            PasswordField(
-              labelText: 'Password',
-              controller: passwordController,
-            ),
-            const SizedBox(
-              height: 35.0,
-            ),
-            _buildSignInButton(context),
-            const SizedBox(
-              height: 15.0,
-            ),
-            isLoading
-                ? const CircularProgressIndicator()
-                : Container(
-                    width: 0, height: 0), // TODO(z0mbie42): remove ugly hack
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 
-  Material _buildSignInButton(BuildContext context) {
+  Material _buildSignInButton(BuildContext context, bool isLoading) {
     return Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(6.0),
@@ -70,44 +91,32 @@ class _SignInState extends State<SignIn> {
         minWidth: MediaQuery.of(context).size.width,
         padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         onPressed: isLoading ? null : _onSignInButtonPressed,
-        child: Text(isLoading ? 'Signing in...' : 'Sign in',
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
+        child: Text(
+          isLoading ? 'Signing in...' : 'Sign in',
+          textAlign: TextAlign.center,
+          style:
+              style.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
 
   Future<void> _onSignInButtonPressed() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    final String message = jsonEncode(AuthGuiSignIn(
-      username: usernameController.text,
-      password: passwordController.text,
-    ));
-
     try {
       final Map<String, dynamic> json =
-          await compute(_SignInState._nativeCall, message);
+          await _bloc.signIn(usernameController.text, passwordController.text);
       debugPrint('$json');
     } catch (err) {
       Scaffold.of(context)
         ..removeCurrentSnackBar()
         ..showSnackBar(
           SnackBar(
-              content: Text(err.toString(),
-                  style: TextStyle(color: Colors.red[300]))),
+            content: Text(
+              err.toString(),
+              style: TextStyle(color: Colors.red[300]),
+            ),
+          ),
         );
-    } finally {
-      setState(() {
-        isLoading = false;
-      });
     }
-  }
-
-  static Map<String, dynamic> _nativeCall(String message) {
-    return coreFfi.call(message);
   }
 }
