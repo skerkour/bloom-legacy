81367f56590ae3a9fb95212c0503e429ad78e9da
z0mbie42 <sylvain@kerkour.fr>
2019-11-23 00:12:40 +0100

    website: /security page. #120

--- website/src/views/security.vue
+++ website/src/views/security.vue
@@ -1,12 +1,33 @@
 <template>
-  <h1>Security</h1>
+  <v-container class="mt-4 text-center">
+    <v-row class="justify-center">
+      <v-col cols="12" sm="10" md="8" class="text-center">
+
+        <h1 class="mt-5 mb-3 display-1 font-weight-medium">Security</h1>
+        <p class="body-1 font-weight-regular mb-5">
+          Keeping our customer data safe and secure is our top priority. We take threats very
+        seriously and work hard to protect our customers and their data. Your input and feedback
+          on our security is greatly appreciated.
+        </p>
+
+        <h2 class="mt-5 mb-3 display-1 font-weight-medium">Reporting Issues</h2>
+
+        <p class="body-1 font-weight-regular">
+          Send reports directly to <a href="mailto:security@bloom.sh">security@bloom.sh</a>
+          and we will get back to you faster than the light.
+        </p>
+
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class Security extends Vue {
+export default class Index extends Vue {
   // props
   // data
   // computed
@@ -15,3 +36,9 @@ export default class Security extends Vue {
   // methods
 }
 </script>
+
+<style scoped lang="scss">
+a {
+  text-decoration: none;
+}
+</style>
