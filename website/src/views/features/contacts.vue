f12b53dd130ae01a1258ea614ef8e21a696a3c27
z0mbie42 <sylvain@kerkour.fr>
2019-11-23 21:51:46 +0100

    websites: /features/contacts #120

--- website/src/views/features/contacts.vue
+++ website/src/views/features/contacts.vue
@@ -1,14 +1,39 @@
 <template>
-  <h1>Contacts</h1>
+  <blm-feature-page
+    name="Contacts"
+    description="Keep your contacts organized and up to date"
+    hero="/static/imgs/features/contacts_hero.svg"
+    :features="features"
+  />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import FeaturePage from '@/components/feature_page.vue';
 
-@Component
-export default class Contacts extends Vue {
+
+@Component({
+  components: {
+    'blm-feature-page': FeaturePage,
+  },
+})
+export default class Drive extends Vue {
   // props
   // data
+  features = [
+    {
+      description: `Safely back up your contacts to the cloud
+            to access from any device.`,
+      img: '/static/imgs/features/contacts_devices.svg',
+      name: 'Back up your contacts and sync them across all your devices',
+    },
+    {
+      description: `Easily add contacts and edit information like phone numbers, emails,
+        and profiles.`,
+      img: '/static/imgs/features/contacts_search.svg',
+      name: 'Keep your contacts organized and up to date',
+    },
+  ];
   // computed
   // lifecycle
   // watch
