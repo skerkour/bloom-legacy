c2b37888306e66cf4e4a7748eff1fe8f2b7ae46e
z0mbie42 <sylvain@kerkour.fr>
2019-11-25 14:18:41 +0100

    website: improve website

--- website/src/main.ts
+++ website/src/main.ts
@@ -4,17 +4,11 @@ import vuetify from '@/plugins/vuetify';
 import App from '@/app.vue';
 import { i18n } from '@/i18n';
 
-const { log, Level } = require('@bloom42/astro');
-
 // init stage dependant stuff
 if (process.env.ENV === 'development') {
   Vue.config.productionTip = true;
 } else {
   Vue.config.productionTip = false;
-
-  if (process.env.ENV === 'production') {
-    log.config({ level: Level.INFO });
-  }
 }
 
 
