1438c90a8ab7816e8252677b698316ca4603ae39
z0mbie42 <sylvain@kerkour.fr>
2019-11-25 17:55:19 +0100

    website: scroll to top on page change

--- website/src/views/routes.ts
+++ website/src/views/routes.ts
@@ -18,6 +18,9 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  },
   routes: [
     {
       path: '/',
