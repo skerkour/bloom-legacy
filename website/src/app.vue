b978d60a7ba80955b3b62bd58a15fb13f180771d
z0mbie42 <sylvain@kerkour.fr>
2019-11-25 18:28:04 +0100

    website: fix app

--- website/src/app.vue
+++ website/src/app.vue
@@ -22,3 +22,69 @@ import Footer from '@/components/footer.vue';
 })
 export default class App extends Vue {}
 </script>
+
+
+<style lang="scss">
+// Global vars
+$grey: #2c3e50;
+$white: #fff;
+$red: #F44336;
+$blue: #2196F3;
+
+$success: #67C23A;
+$info: #409EFF;
+$warning: #E6A23C;
+$danger: #F56C6C;
+
+@font-face {
+  font-family: "rounded_elegance";
+  src: url("/static/fonts/rounded_elegance.ttf") format("truetype");
+}
+
+a {
+  text-decoration: none;
+  color: none;
+}
+
+.blm-rounded-elegance {
+  font-family: rounded_elegance !important;
+  font-size: unset;
+}
+
+.v-btn {
+  border-radius: 4px;
+  text-transform: none; /* overwrite default 'uppercase' */
+}
+
+// not sure why it does not work in block below
+.theme--dark.v-application {
+  background-color: #303030;
+}
+.theme--dark {
+  .v-toolbar, .v-table, .v-footer, .v-tabs__bar, .v-application {
+    background-color: #303030;
+  }
+
+  pre {
+    background-color: #212121;
+  }
+}
+
+// not sure why it does not work in block below
+.theme--light.v-application {
+  background-color: #fefefe;
+}
+.theme--light {
+  .v-toolbar, .v-table, .v-footer, .v-tabs__bar, .v-application {
+    background-color: #fefefe;
+  }
+
+  pre {
+    background-color: #f0f0f0;
+  }
+}
+
+.blm-pointer {
+  cursor: pointer;
+}
+</style>
