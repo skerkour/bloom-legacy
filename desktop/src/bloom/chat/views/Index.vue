0859c4048cb60d7dcd65103143808e3542a8d449
z0mbie42 <sylvain@kerkour.fr>
2019-12-10 17:16:12 +0100

    desktop: chat

--- desktop/src/bloom/chat/views/Index.vue
+++ desktop/src/bloom/chat/views/Index.vue
@@ -4,23 +4,19 @@
       <v-col cols="3" class="blm-scrollable">
         <v-list two-line avatar>
         <v-list-item-group v-model="selectedConversation" color="primary">
-          <v-list-item
-            v-for="(conversation, i) in conversations"
-            :key="i"
-          >
+          <v-list-item v-for="(conversation, i) in conversations" :key="i">
             <v-list-item-avatar>
               <v-img :src="conversation.avatar"></v-img>
             </v-list-item-avatar>
             <v-list-item-content class="text-left">
               <v-list-item-title v-html="conversation.title"></v-list-item-title>
-              <v-list-item-subtitle v-html="conversation.subtitle"></v-list-item-subtitle>
+              <v-list-item-subtitle>{{ conversation.subtitle }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
 
       </v-col>
-
 
       <v-col cols="9" class="blm-chat-container">
         <h1>Chat</h1>
@@ -43,77 +39,77 @@ export default class Index extends Vue {
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
       title: 'Brunch this weekend?',
-      subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+      subtitle: 'I\'ll be in your neighborhood doing errands this weekend. Do you want to hang out?',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
       title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-      subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+      subtitle: 'Wish I could come, but I\'m out of town this weekend.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
       title: 'Oui oui',
-      subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+      subtitle: 'Do you have Paris recommendations? Have you ever been?',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
       title: 'Birthday gift',
-      subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+      subtitle: 'Have any ideas about what we should get Heidi for her birthday?',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
     {
       avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
       title: 'Recipe to try',
-      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
     },
 
   ];
