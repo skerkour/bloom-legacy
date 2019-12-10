33d713333400bda23509f3555e73dc17828964c2
z0mbie42 <sylvain@kerkour.fr>
2019-12-10 14:19:05 +0100

    desktop: chat

--- desktop/src/bloom/chat/views/Index.vue
+++ desktop/src/bloom/chat/views/Index.vue
@@ -1,6 +1,31 @@
 <template>
   <v-container fluid>
-    <h1>Chat</h1>
+    <v-row>
+      <v-col cols="3">
+        <v-list two-line avatar>
+        <v-list-item-group v-model="conversation" color="primary">
+          <v-list-item
+            v-for="(conversation, i) in conversations"
+            :key="i"
+          >
+            <v-list-item-avatar>
+              <v-img :src="conversation.avatar"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content class="text-left">
+              <v-list-item-title v-html="conversation.title"></v-list-item-title>
+              <v-list-item-subtitle v-html="conversation.subtitle"></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+
+      </v-col>
+
+
+      <v-col cols="9">
+        <h1>Chat</h1>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -13,6 +38,35 @@ import { Component, Vue } from 'vue-property-decorator';
 export default class Index extends Vue {
   // props
   // data
+  selectedConversation = 1;
+  conversations = [
+    {
+      avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+      title: 'Brunch this weekend?',
+      subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+    },
+    {
+      avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+      title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+      subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+    },
+    {
+      avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+      title: 'Oui oui',
+      subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+    },
+    {
+      avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+      title: 'Birthday gift',
+      subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+    },
+    {
+      avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+      title: 'Recipe to try',
+      subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+    },
+  ];
+
   // computed
   // lifecycle
   // watch
