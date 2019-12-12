1bbf52c96d106d2c295bc3b8e4700d0daf7fc137
z0mbie42 <sylvain@kerkour.fr>
2019-12-12 14:41:37 +0100

    desktop/myaccount: security

--- desktop/src/bloom/myaccount/views/security.vue
+++ desktop/src/bloom/myaccount/views/security.vue
@@ -1,17 +1,44 @@
 <template>
-  <v-container fluid>
-    <h1>Security</h1>
+  <v-container fluid class="text-left">
+
+    <v-alert icon="mdi-alert-circle" type="error" :value="error !== ''">
+      {{ error }}
+    </v-alert>
+
+    <v-row>
+      <v-col cols="12">
+        <div class="headline">
+          Change password
+        </div>
+        <div>
+          All other devices will be disconnected
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="9">
+        <blm-myaccount-form-change-password />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import ChangePasswordForm from '../components/change_password_form.vue';
 
-@Component
+@Component({
+  components: {
+    'blm-myaccount-form-change-password': ChangePasswordForm,
+  },
+})
 export default class Security extends Vue {
   // props
   // data
+  error = '';
+
   // computed
   // lifecycle
   // watch
