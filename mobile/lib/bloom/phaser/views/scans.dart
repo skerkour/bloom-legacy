e0a09c9c68e9214573114b35018a99c6dec0391a
z0mbie42 <sylvain@kerkour.fr>
2019-12-13 17:01:52 +0100

    mobile/phaser: scans

--- mobile/lib/bloom/phaser/views/scans.dart
+++ mobile/lib/bloom/phaser/views/scans.dart
@@ -16,7 +16,103 @@ class _PhaserScansState extends State<PhaserScansView> {
       appBar: AppBar(
         title: const Text('Phaser'),
       ),
-      body: const Center(child: Text('Phaser')),
+      body: _buildBody(),
     );
+  }
+
+  Widget _buildBody() {
+    final List<Scan> users = Scan.getScans();
+    final List<DataRow> rows = users
+        .map(
+          (Scan user) => DataRow(cells: <DataCell>[
+            DataCell(
+              Text(user.username),
+            ),
+            DataCell(
+              Text(user.email),
+            ),
+            DataCell(
+              Text(user.state),
+            ),
+            DataCell(
+              Text(user.state),
+            ),
+            DataCell(
+              Text(user.state),
+            ),
+          ]),
+        )
+        .toList();
+
+    final List<DataColumn> columns = <DataColumn>[
+      const DataColumn(
+        label: Text('Scan'),
+        numeric: false,
+      ),
+      const DataColumn(
+        label: Text('Email'),
+        numeric: false,
+      ),
+      const DataColumn(
+        label: Text('Active'),
+        numeric: false,
+      ),
+      const DataColumn(
+        label: Text('Active'),
+        numeric: false,
+      ),
+      const DataColumn(
+        label: Text('Active'),
+        numeric: false,
+      ),
+    ];
+
+    return Container(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            rows: rows,
+            columns: columns,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Scan {
+  Scan({this.username, this.email}) {
+    state = 'Active';
+  }
+
+  String username;
+  String email;
+  String state;
+
+  static List<Scan> getScans() {
+    return <Scan>[
+      Scan(username: 'Aaryan', email: 'Shah'),
+      Scan(username: 'Ben', email: 'John'),
+      Scan(username: 'Carrie', email: 'Brown'),
+      Scan(username: 'Deep', email: 'Sen'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Emily', email: 'Jane'),
+      Scan(username: 'Deep', email: 'Sen'),
+    ];
   }
 }
