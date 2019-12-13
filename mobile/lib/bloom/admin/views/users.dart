4996619b4c12ef71a9d0067d9eedd599ad5c7105
z0mbie42 <sylvain@kerkour.fr>
2019-12-13 16:35:44 +0100

    mobile/admin: users

--- mobile/lib/bloom/admin/views/users.dart
+++ mobile/lib/bloom/admin/views/users.dart
@@ -16,7 +16,114 @@ class _AdminUsersState extends State<AdminUsersView> {
       appBar: AppBar(
         title: const Text('Admin'),
       ),
-      body: const Center(child: Text('Users')),
+      body: _buildBody(),
     );
+  }
+
+  Widget _buildBody() {
+    final List<User> users = User.getUsers();
+    final List<DataRow> rows = users
+        .map(
+          (User user) => DataRow(cells: <DataCell>[
+            DataCell(
+              Text(user.username),
+            ),
+            DataCell(
+              Text(user.email),
+            ),
+            DataCell(
+              Text(user.active),
+            ),
+            DataCell(
+              Text(user.active),
+            ),
+            DataCell(
+              Text(user.active),
+            ),
+          ]),
+        )
+        .toList();
+
+    final List<DataColumn> columns = <DataColumn>[
+      const DataColumn(
+        label: Text('Username'),
+        numeric: false,
+      ),
+      const DataColumn(
+        label: Text('Email'),
+        numeric: false,
+      ),
+      const DataColumn(
+        label: Text('Active'),
+        numeric: false,
+      ),
+      const DataColumn(
+        label: Text('Active'),
+        numeric: false,
+      ),
+      const DataColumn(
+        label: Text('Active'),
+        numeric: false,
+      ),
+    ];
+
+    return Container(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            rows: rows,
+            columns: columns,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class User {
+  User({this.username, this.email}) {
+    active = 'Active';
+  }
+
+  String username;
+  String email;
+  String active;
+
+  static List<User> getUsers() {
+    return <User>[
+      User(username: 'Aaryan', email: 'Shah'),
+      User(username: 'Ben', email: 'John'),
+      User(username: 'Carrie', email: 'Brown'),
+      User(username: 'Deep', email: 'Sen'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Emily', email: 'Jane'),
+      User(username: 'Deep', email: 'Sen'),
+    ];
   }
 }
