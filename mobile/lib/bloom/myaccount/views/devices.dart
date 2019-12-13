3001422d490394db521f351ef6a2c0a6dc2d73ff
z0mbie42 <sylvain@kerkour.fr>
2019-12-13 21:10:11 +0100

    mobile/myaccount: devices

--- mobile/lib/bloom/myaccount/views/devices.dart
+++ mobile/lib/bloom/myaccount/views/devices.dart
@@ -14,9 +14,50 @@ class _MyAccountDevicesState extends State<MyAccountDevicesView> {
     return Scaffold(
       drawer: const MyAccountDrawer(),
       appBar: AppBar(
-        title: const Text('MyAccount'),
+        title: const Text('My Account'),
       ),
-      body: const Center(child: Text('MyAccountDevices')),
+      body: _buildBody(),
     );
+  }
+
+  Widget _buildBody() {
+    final List<Device> devices = Device.getDevices();
+
+    return Container(
+      child: ListView.builder(
+          itemCount: devices.length,
+          itemBuilder: (BuildContext context, int index) {
+            final Device device = devices[index];
+
+            return Card(
+              child: ListTile(
+                leading: Icon(Icons.phone_android),
+                title: Text(device.name),
+                subtitle: Text('${device.createdAt} - ${device.ip}'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class Device {
+  Device({this.type, this.name}) {
+    actions = 'Active';
+    createdAt = DateTime.now().toIso8601String();
+    ip = '127.0.0.1';
+  }
+
+  String type;
+  String ip;
+  String actions;
+  String createdAt;
+  String name;
+
+  static List<Device> getDevices() {
+    return <Device>[
+      Device(type: 'Mobile', name: 'My Phone'),
+    ];
   }
 }
