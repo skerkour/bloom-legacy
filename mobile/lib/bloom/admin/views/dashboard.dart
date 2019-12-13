67726eb00115bbe577a80fcf0f84c3346df150ad
z0mbie42 <sylvain@kerkour.fr>
2019-12-13 15:33:28 +0100

    mobile/admin: dashboard

--- mobile/lib/bloom/admin/views/dashboard.dart
+++ mobile/lib/bloom/admin/views/dashboard.dart
@@ -1,4 +1,6 @@
+import 'package:bloom/bloom/admin/views/users.dart';
 import 'package:bloom/bloom/admin/widgets/drawer.dart';
+import 'package:bloom/bloom/const.dart';
 import 'package:flutter/material.dart';
 
 class AdminDashboardView extends StatefulWidget {
@@ -9,6 +11,9 @@ class AdminDashboardView extends StatefulWidget {
 }
 
 class _AdminDashboardState extends State<AdminDashboardView> {
+  String _version = '1.0.0';
+  int _users = 9000;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +21,34 @@ class _AdminDashboardState extends State<AdminDashboardView> {
       appBar: AppBar(
         title: const Text('Admin'),
       ),
-      body: const Center(child: Text('Admin')),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: ListView(children: <Widget>[
+        ListTile(
+          title: const Text('Bloom'),
+          subtitle: Text('v$_version'),
+        ),
+        const Divider(),
+        ListTile(
+          title: const Text('Users'),
+          subtitle: Text('$_users'),
+          onTap: () {
+            Navigator.pushAndRemoveUntil<dynamic>(
+              context,
+              MaterialPageRoute<dynamic>(
+                builder: (BuildContext context) => const AdminUsersView(),
+              ),
+              (Route<dynamic> route) => route.settings.name == PATH_ADMIN,
+            );
+          },
+        ),
+        const Divider(),
+      ]),
     );
   }
 }
