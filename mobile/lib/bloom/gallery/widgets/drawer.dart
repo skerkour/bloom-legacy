d1f44b8a1c79fec8cd8f9142a042ef7b3ad9c17c
z0mbie42 <sylvain@kerkour.fr>
2019-12-17 16:13:37 +0100

    mobile/gallery: albums

--- mobile/lib/bloom/gallery/widgets/drawer.dart
+++ mobile/lib/bloom/gallery/widgets/drawer.dart
@@ -1,3 +1,5 @@
+import 'package:bloom/bloom/const.dart';
+import 'package:bloom/bloom/gallery/views/albums.dart';
 import 'package:flutter/material.dart';
 
 class GalleryDrawer extends StatefulWidget {
@@ -23,9 +25,30 @@ class _GalleryDrawerState extends State<GalleryDrawer> {
           ),
           const Divider(),
           ListTile(
-            leading: const Icon(Icons.photo),
-            title: const Text('Gallery'),
-            onTap: () => debugPrint('Gallery tapped'),
+            leading: const Icon(Icons.photo_library),
+            title: const Text('Galley'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                PATH_GALLERY,
+                (Route<dynamic> route) => route.settings.name == '/',
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.photo_album),
+            title: const Text('Albums'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushAndRemoveUntil<dynamic>(
+                context,
+                MaterialPageRoute<dynamic>(
+                  builder: (BuildContext context) => const GalleryAlbumsView(),
+                ),
+                (Route<dynamic> route) => route.settings.name == PATH_GALLERY,
+              );
+            },
           ),
         ],
       ),
