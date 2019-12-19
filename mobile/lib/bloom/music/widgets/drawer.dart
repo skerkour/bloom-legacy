c99e9d646d55faa217a680eff8bd1d66a783603d
z0mbie42 <sylvain@kerkour.fr>
2019-12-19 12:33:31 +0100

    mobile/music: Artists + albums

--- mobile/lib/bloom/music/widgets/drawer.dart
+++ mobile/lib/bloom/music/widgets/drawer.dart
@@ -1,4 +1,6 @@
 import 'package:bloom/bloom/const.dart';
+import 'package:bloom/bloom/music/views/albums.dart';
+import 'package:bloom/bloom/music/views/artists.dart';
 import 'package:bloom/bloom/music/views/playlists.dart';
 import 'package:flutter/material.dart';
 
@@ -33,6 +35,34 @@ class _MusicDrawerState extends State<MusicDrawer> {
                 context,
                 PATH_MUSIC,
                 (Route<dynamic> route) => route.settings.name == '/',
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.library_music),
+            title: const Text('Artists'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushAndRemoveUntil<dynamic>(
+                context,
+                MaterialPageRoute<dynamic>(
+                  builder: (BuildContext context) => const MusicArtistsView(),
+                ),
+                (Route<dynamic> route) => route.settings.name == PATH_MUSIC,
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.music_video),
+            title: const Text('Albums'),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushAndRemoveUntil<dynamic>(
+                context,
+                MaterialPageRoute<dynamic>(
+                  builder: (BuildContext context) => const MusicAlbumsView(),
+                ),
+                (Route<dynamic> route) => route.settings.name == PATH_MUSIC,
               );
             },
           ),
