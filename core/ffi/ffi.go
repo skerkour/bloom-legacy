1929fa69bee24deb38325622f2e9bc75824966e8
z0mbie42 <sylvain@kerkour.fr>
2020-01-06 23:02:33 +0100

    editor config for go

--- core/ffi/ffi.go
+++ core/ffi/ffi.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-  "C"
-  "encoding/json"
+	"C"
+	"encoding/json"
 )
 
 type Payload struct {
-  Method string `json:"method"`
-  Params json.RawMessage `json:"params"`
+	Method string          `json:"method"`
+	Params json.RawMessage `json:"params"`
+}
+
+type Error struct {
+	Code string      `json:"code"`
+	Msg  string      `json:"msg"`
+	Meta interface{} `json:"meta"`
 }
 
 //export call
 func Call(message *C.char) *C.char {
-  return nil
+	return nil
 }
 
-func main(){}
+func main() {}
