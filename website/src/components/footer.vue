f199d0e3e0cee73b3ba113f819e2407b416501a3
z0mbie42 <sylvain@kerkour.fr>
2020-01-07 15:08:55 +0100

    website: reupdate footer

--- website/src/components/footer.vue
+++ website/src/components/footer.vue
@@ -18,8 +18,8 @@
         <v-col cols="12" sm="4" class="pa-4">
           <h5 class="title pb-3">{{ $t('footer.product.title') }}</h5>
           <ul>
-            <!-- <li><router-link to="/faq">{{ $t('footer.product.faq') }}</router-link></li> -->
-            <!-- <li><router-link to="/help">{{ $t('footer.product.help') }}</router-link></li> -->
+            <li><router-link to="/faq">{{ $t('footer.product.faq') }}</router-link></li>
+            <li><router-link to="/help">{{ $t('footer.product.help') }}</router-link></li>
             <li><router-link class="nav-link" to="/download">{{ $t('footer.product.download') }}</router-link></li>
             <li><a class="nav-link" target="_blank" rel="noopener" href="https://gitlab.com/bloom42/wiki/-/wikis/organization/contributing">{{ $t('footer.product.contribute') }}</a></li>
             <li><a class="nav-link" target="_blank" rel="noopener" href="https://gitlab.com/bloom42/bloom/-/wikis">{{ $t('footer.product.documentation') }}</a></li>
