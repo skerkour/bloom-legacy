324dcb5dc06432ef98305bed4fc181e7b7deb120
z0mbie42 <sylvain@kerkour.fr>
2020-01-07 15:01:03 +0100

    website: redirect in index.html

--- website/src/main.ts
+++ website/src/main.ts
@@ -4,15 +4,6 @@ import vuetify from '@/plugins/vuetify';
 import App from '@/app.vue';
 import { i18n } from '@/i18n';
 
-// because GitLab pages does not support server side redirects
-function redirect(domain: string) {
-  const { hostname } = window.location;
-  if (hostname !== domain && hostname !== 'localhost' && hostname !== '127.0.0.1') {
-    window.location.href = `https://${domain}`;
-  }
-}
-redirect('bloom.sh');
-
 // init stage dependant stuff
 if (process.env.ENV === 'development') {
   Vue.config.productionTip = true;
