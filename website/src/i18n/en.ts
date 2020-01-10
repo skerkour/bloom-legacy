617f9567ee0438dbd852d749cdb00b68826af0f3
z0mbie42 <sylvain@kerkour.fr>
2020-01-10 21:32:26 +0100

    website: i18n /features/music

--- website/src/i18n/en.ts
+++ website/src/i18n/en.ts
@@ -122,6 +122,25 @@ export default {
       },
     },
     music_description: 'Never stop enjoying your music! Directly from your drive.',
+    music: {
+      description: 'A world of emotion. <br/> Available wherever you are.',
+      feature1: {
+        name: 'Play from any device',
+        description: `You can use Bloom Music to listen to your library on your computer,
+        smartphone, or other supported devices.`,
+      },
+      feature2: {
+        name: 'Stream your personal music library wherever you are',
+        description: `Whether you've got plenty of rare tunes ripped from vinyls, a bunch of albums
+        from indie artists on Bandcamp, or a carefully curated collection of lossless tracks,
+        Bloom Music allows you to steam them from wherever you are.`,
+      },
+      feature3: {
+        name: 'Directly from your drive',
+        description: `Thanks to the unified data model of Bloom, all your music files in your Bloom
+        Drive are directly available in Bloom Music. No need to reorganize everything everytime.`,
+      },
+    },
     calendar_description: 'Get organized on the go with the Bloom Calendar!',
     notes_description: `Quickly capture what’s on your mind and get a reminder later
     at the right place or time.`,
