5c7f78a162960c843956417c26387133e9599173
z0mbie42 <sylvain@kerkour.fr>
2020-01-11 18:10:02 +0100

    core: improve message handling

--- core/ffi/ffi.go
+++ core/ffi/ffi.go
@@ -3,6 +3,7 @@ package main
 import (
 	"C"
 	"encoding/json"
+	"strings"
 )
 
 type Payload struct {
@@ -11,9 +12,9 @@ type Payload struct {
 }
 
 type Error struct {
-	Code string      `json:"code"`
-	Message  string  `json:"message"`
-	Meta interface{} `json:"meta"`
+	Code    string      `json:"code"`
+	Message string      `json:"message"`
+	Meta    interface{} `json:"meta"`
 }
 
 //export blmcore_call
@@ -30,7 +31,24 @@ func blmcore_call(message *C.char) *C.char {
 		return nil // TODO(z0mbie42): return error
 	}
 
-	switch payload.Method {
+	parts := strings.Split(payload.Method, ".")
+	if len(parts) != 2 {
+		return nil // TODO(z0mbie42): return error
+	}
+
+	// TODO(z0mbie42): handle methods returns go error, and convert to c error here
+	switch parts[0] {
+	case "notes":
+		return handleNotesMethod(parts[1], payload.Params)
+	default:
+		return nil // TODO: return not found error
+	}
+}
+
+func handleNotesMethod(method string, params json.RawMessage) *C.char {
+	switch method {
+	case "list_notes":
+		return C.CString(`{"data": { "notes": []}}`)
 	default:
 		return nil // TODO: return not found error
 	}
