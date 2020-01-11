5a047c0430c8edce1c401e070746ef0de8924050
z0mbie42 <sylvain@kerkour.fr>
2020-01-11 21:41:07 +0100

    core: calc

--- core/bloom/calculator/calc.go
+++ core/bloom/calculator/calc.go
@@ -1,5 +1,57 @@
 package calculator
 
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strconv"
+)
+
+func evalExpr(exp ast.Expr) int {
+	switch exp := exp.(type) {
+	case *ast.BinaryExpr:
+		return evalBinaryExpr(exp)
+	case *ast.BasicLit:
+		switch exp.Kind {
+		case token.INT:
+			i, _ := strconv.Atoi(exp.Value)
+			return i
+		}
+	}
+
+	return 0
+}
+
+func evalBinaryExpr(exp *ast.BinaryExpr) int {
+	left := evalExpr(exp.X)
+	right := evalExpr(exp.Y)
+
+	switch exp.Op {
+	case token.ADD:
+		return left + right
+	case token.SUB:
+		return left - right
+	case token.MUL:
+		return left * right
+	case token.QUO:
+		return left / right
+	}
+
+	return 0
+}
+
+func Eval(expression string) (int, error) {
+	exp, err := parser.ParseExpr(expression)
+	if err != nil {
+		return 0, err
+	}
+	return evalExpr(exp), nil
+}
+
 func Calc(params CalcParams) (CalcResult, error) {
-	return CalcResult{Result: params.Expression}, nil
+	res, err := Eval(params.Expression)
+	if err != nil {
+		return CalcResult{}, err
+	}
+	return CalcResult{Result: strconv.Itoa(res)}, nil
 }
