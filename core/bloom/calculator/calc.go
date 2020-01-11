142286b8edb4f5f59be46c8075573c58a0fed85f
z0mbie42 <sylvain@kerkour.fr>
2020-01-11 20:52:36 +0100

    core: refactoring

--- core/bloom/calculator/calc.go
+++ core/bloom/calculator/calc.go
@@ -1,13 +1,5 @@
 package calculator
 
-type CalcParams struct {
-	Expression string `json:"expression"`
-}
-
-type CalcResult struct {
-	Result string `json:"result"`
-}
-
 func Calc(params CalcParams) (CalcResult, error) {
 	return CalcResult{Result: params.Expression}, nil
 }
--- core/bloom/notes/models.go
+++ core/bloom/notes/models.go
@@ -1,4 +1,4 @@
-package models
+package notes
 
 type Note struct {
 }
