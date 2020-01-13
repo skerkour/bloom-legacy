9287fe0619c1eb5e959b7aa1e32544faa531e20b
z0mbie42 <sylvain@kerkour.fr>
2020-01-13 21:38:26 +0100

    core: move consts

---
+++ core/consts/const.go
@@ -0,0 +1,7 @@
+package consts
+
+const ADDRESS_HELLO = "hello@bloom.sh"
+const ADDRESS_NOTIFY = "notify@bloom.sh"
+const ADDRESS_SECURITY = "security@bloom.sh"
+
+const API_BASE_URL = "http://192.168.1.53:8000"
--- core_old/bloom/const/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod accounts;
-pub mod auth;
-pub mod db;
-
-pub static ADDRESS_HELLO: &str = "hello@bloom.sh";
-pub static ADDRESS_NOTIFY: &str = "notify@bloom.sh";
-pub static ADDRESS_SECURITY: &str = "security@bloom.sh";
-
-pub const API_URL: &str = "http://192.168.1.53:8000/";
