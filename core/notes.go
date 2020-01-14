a6e8911a1a0f921a351208a66ff275776e7bb5b2
z0mbie42 <sylvain@kerkour.fr>
2020-01-14 17:56:35 +0100

    core/notes: List archived

--- core/notes.go
+++ core/notes.go
@@ -21,6 +21,17 @@ func HandleNotesMethod(method string, jsonParams json.RawMessage) MessageOut {
 			return InternalError(err) // TODO(z0mbie42): return error
 		}
 		return MessageOut{Data: res}
+	case "list_archived":
+		var params kernel.Empty
+		err := json.Unmarshal(jsonParams, &params)
+		if err != nil {
+			return InternalError(err) // TODO(z0mbie42): return error
+		}
+		res, err := notes.ListArchived(params)
+		if err != nil {
+			return InternalError(err) // TODO(z0mbie42): return error
+		}
+		return MessageOut{Data: res}
 	default:
 		return MethodNotFoundError(method, "notes")
 	}
