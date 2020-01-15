a365df901f8f752cf70413627bc101bb89fe7ca8
z0mbie42 <sylvain@kerkour.fr>
2020-01-15 23:23:15 +0100

    server: star middlewares

---
+++ server/server/middleswares.go
@@ -0,0 +1,14 @@
+package server
+
+import (
+	"net/http"
+)
+
+func SecurityHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "Deny")
+		w.Header().Set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
+		next.ServeHTTP(w, r)
+	})
+}
