203f9716fd4b687129b6957e44e9fd8bd6f9f013
z0mbie42 <sylvain@kerkour.fr>
2020-01-16 19:43:48 +0100

    website: reorder footer

--- website/src/components/footer.vue
+++ website/src/components/footer.vue
@@ -32,8 +32,8 @@
           <ul>
             <li><a class="nav-link" href="https://fatalentropy.com" target="_blank" rel="noopener">Blog</a></li>
             <li><a class="nav-link" href="https://gitlab.com/bloom42/forum" target="_blank" rel="noopener">Forum</a></li>
-            <li><a class="nav-link" href="https://gitlab.com/bloom42" target="_blank" rel="noopener">GitLab</a></li>
             <li><a class="nav-link" href="https://twitter.com/42bloom" target="_blank" rel="noopener">Twitter</a></li>
+            <li><a class="nav-link" href="https://gitlab.com/bloom42" target="_blank" rel="noopener">GitLab</a></li>
             <li><a class="nav-link" href="https://mastodon.social/@42bloom" target="_blank" rel="noopener">Mastodon</a></li>
           </ul>
         </v-col>
