6f2b07b1440a6bbedb1fc57ff5fec1bb5a524509
z0mbie42 <sylvain@kerkour.fr>
2020-01-16 15:16:51 +0100

    server: accounts/sign_out

--- server/bloom/accounts/sign_in.go
+++ server/bloom/accounts/sign_in.go
@@ -1,1 +1,14 @@
 package accounts
+
+import (
+	"context"
+
+	rpcaccounts "gitlab.com/bloom42/bloom/core/rpc/accounts"
+)
+
+func (s Handler) SignIn(ctx context.Context, params *rpcaccounts.SignInParams) (*rpcaccounts.Session, error) {
+	return &rpcaccounts.Session{
+		Id:    "MyRandomId",
+		Token: "MyRandomToken",
+	}, nil
+}
