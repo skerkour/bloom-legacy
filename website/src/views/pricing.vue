2dc27a8bc36ff0f6ecf1561bd28ed7e087df1810
z0mbie42 <sylvain@kerkour.fr>
2020-01-16 19:09:32 +0100

    website: pricing

--- website/src/views/pricing.vue
+++ website/src/views/pricing.vue
@@ -2,26 +2,72 @@
   <v-container class="mt-3 text-center">
 
     <v-row class="justify-center">
-      <v-col cols="12" sm="10" md="8" class="text-center mt-5">
+      <v-col cols="10" sm="8" md="10" class="text-center mt-5">
         <h1 class="display-3 mb-5">Pricing</h1>
+        <v-row class="justify-center">
+          <v-col cols="12" md="3" class="text-center mt-5">
+            <v-card class="mx-auto" outlined>
+              <v-card-title class="text-center justify-center">Free</v-card-title>
+              <v-card-text>
+                blah blah blah
+              </v-card-text>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="3" class="text-center mt-5">
+            <v-card class="mx-auto" outlined>
+              <v-card-title class="text-center justify-center">Basic</v-card-title>
+              <v-card-text>
+                blah blah blah
+              </v-card-text>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="3" class="text-center mt-5">
+            <v-card class="mx-auto" outlined>
+              <v-card-title class="text-center justify-center">Pro</v-card-title>
+              <v-card-text>
+                blah blah blah
+              </v-card-text>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="3" class="text-center mt-5">
+            <v-card class="mx-auto" outlined>
+              <v-card-title class="text-center justify-center">Ultra</v-card-title>
+              <v-card-text>
+                blah blah blah
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
 
     <v-row class="justify-center">
       <v-col cols="12" sm="10" md="8" class="text-center mt-5">
-        <h1 class="display-3 mb-5">Save big with Bloom.</h1>
+        <blm-download-btn />
+        <blm-other-downloads-link />
+      </v-col>
+    </v-row>
+
+    <v-row class="justify-center">
+      <v-col cols="12" sm="10" md="8" class="text-center mt-5">
+        <h1 class="display-3 mb-5">Save big with Bloom</h1>
       </v-col>
     </v-row>
 
     <v-row class="justify-center">
       <v-col cols="12" sm="10" md="8" class="text-center mt-5">
         <h1 class="display-3 mb-5">Having questions?</h1>
-        <router-link outlined to="/faq">Explore the FAQ</router-link>
+        <div class="mt-5 pt-5">
+          <router-link outlined to="/faq" >Explore the FAQ</router-link>
+        </div>
       </v-col>
     </v-row>
 
     <v-row class="justify-center">
-      <v-col cols="12" sm="10" md="8" class="text-center mt-5">
+      <v-col cols="12" sm="10" md="8" class="text-center">
         <blm-download-btn />
         <blm-other-downloads-link />
       </v-col>
