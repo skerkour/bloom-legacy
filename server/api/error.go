b311ab46f1c255e1460266b788de234cb4a47dea
z0mbie42 <sylvain@kerkour.fr>
2020-01-16 22:36:48 +0100

    server: ResError

--- server/api/error.go
+++ server/api/error.go
@@ -1,7 +1,26 @@
 package api
 
+import (
+	"encoding/json"
+	"gitlab.com/bloom42/libs/rz-go"
+	"net/http"
+)
+
 type Error struct {
 	Code    string            `json:"code"`
 	Message string            `json:"msg"`
 	Meta    map[string]string `json:"meta"`
+}
+
+func ResError(w http.ResponseWriter, r *http.Request, status int, erro Error) {
+	if status == 500 {
+		erro.Message = "internal error, please try again later"
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	err := json.NewEncoder(w).Encode(erro)
+	if err != nil {
+		rz.FromCtx(r.Context()).Error("encoding error response to JSON", rz.Err(err))
+	}
 }
--- server/api/not_found.go
+++ server/api/not_found.go
@@ -1,24 +1,16 @@
 package api
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := json.Marshal(Error{
+	err := Error{
 		Code:    "not_found",
 		Message: "route not found",
 		Meta: map[string]string{
 			"path": r.URL.Path,
 		},
-	})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
-	w.Header().Set("content-type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-	w.Write(data)
-
+	ResError(w, r, 404, err)
 }
