5273e85e062ea4cbfbff1b7d3afda0c297f65e11
z0mbie42 <sylvain@kerkour.fr>
2020-01-16 22:33:04 +0100

    server; api fix ResJSON

--- server/api/util.go
+++ server/api/util.go
@@ -6,23 +6,15 @@ import (
 	"net/http"
 )
 
-type Response struct {
-	Error  *string     `json:"error"`
-	Data   interface{} `json:"data"`
-	Status int         `json:"status"`
-}
-
 // ResJSON write json response
 func ResJSON(w http.ResponseWriter, r *http.Request, status int, payload interface{}) {
-	res := Response{Data: payload, Status: status}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	err := json.NewEncoder(w).Encode(res)
+	err := json.NewEncoder(w).Encode(payload)
 	if err != nil {
 		rz.FromCtx(r.Context()).Error("encoding response to JSON", rz.Err(err))
 		errorMessage := "internal error, please try again later"
-		res := Response{Error: &errorMessage, Status: status}
+		res := Error{Message: errorMessage, Code: "internal"}
 		w.WriteHeader(500)
 		_ = json.NewEncoder(w).Encode(res)
 		return
