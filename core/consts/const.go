fc1ca40b4cf6679830877f1c00648eabf7f7ac31
z0mbie42 <sylvain@kerkour.fr>
2020-01-17 17:38:54 +0100

    core: consts

--- core/consts/const.go
+++ core/consts/const.go
@@ -5,3 +5,9 @@ const ADDRESS_NOTIFY = "notify@bloom.sh"
 const ADDRESS_SECURITY = "security@bloom.sh"
 
 const API_BASE_URL = "http://192.168.1.53:8000"
+
+const (
+	EN_PRODUCTION   = "production"
+	ENV_STAGING     = "staging"
+	ENV_DEVELOPMENT = "development"
+)
