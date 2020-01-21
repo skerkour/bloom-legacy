6c59ec751042ca851c356cdb8efedd2dc96cc8ab
z0mbie42 <sylvain@kerkour.fr>
2020-01-21 15:31:23 +0100

    website: faq

--- website/src/views/faq.vue
+++ website/src/views/faq.vue
@@ -9,10 +9,10 @@
 
     <v-row class="justify-center">
       <v-col cols="12" sm="10" md="8" class="text-left m-3">
-        <div class="mb-5 mt-5" v-for="question in questions" :key="question.question">
-          <h2 class="display-2 mb-2">{{ question.question }}</h2>
+        <div class="mb-5 mt-5" v-for="(question, index) in questions" :key="index">
+          <h2 class="display-1 font-weight-medium mb-2">{{ question.question }}</h2>
           <p v-html="question.answer"></p>
-          <v-divider />
+          <v-divider v-if="index !== questions.length - 1"/>
         </div>
       </v-col>
 
@@ -46,40 +46,50 @@ export default class Pricing extends Vue {
   // data
   questions = [
     {
-      question: 'How are my data secured?',
+      question: 'How secure are my data?',
       answer: `Some of your most personal moments are shared on WhatsApp, which is why we built end-to-end encryption.
-      We use state of the art cryptography (
-        <a href="https://tools.ietf.org/html/draft-irtf-cfrg-xchacha-03" target="_blank" rel="noopener noreferer">XChaCha20Poly1305</a>,
+      We use state of the art cryptography
+        (<a href="https://tools.ietf.org/html/draft-irtf-cfrg-xchacha-03" target="_blank" rel="noopener noreferer">XChaCha20Poly1305</a>,
         <a href="https://en.wikipedia.org/wiki/Argon2" target="_blank" rel="noopener noreferer">Argon2id</a>,
         <a href="https://en.wikipedia.org/wiki/BLAKE_(hash_function)" target="_blank" rel="noopener noreferer">Blake2b</a>)
         to securely store your all your data.
-      Your data are encrypted on your own devices and (even if we wanted) we can't read them.`,
+      Your data are encrypted on your own devices and (even if we wanted) we can't read them. <br />
+      Also, we do not store financial data ourselves, we use  <a href="https://stripe.com" target="_blank" rel="noopener noreferer">Stripe</a>.`,
     },
     {
-      question: 'Where the encrpyted data are stored?',
-      answer: 'The encrypted data are stored in a Scaleway Datacenter in France.',
+      question: 'Where the encrypted data are stored?',
+      answer: `The (<a href="https://en.wikipedia.org/wiki/End-to-end_encryption" target="_blank" rel="noopener noreferer">end-to-end</a>)
+        encrypted data are stored in a  <a href="scaleway.com" target="_blank" rel="noopener noreferer">Scaleway</a>
+        datacenter in a privacy friendly country: France.`,
     },
     {
       question: 'Why Bloom is more ethical than other service providers?',
-      answer: 'Bloom is <b>100% Open Source</b>. Anybody can audit the can -> virtue',
+      answer: `Bloom is <b>100% Open Source</b>. Anyone can inspect how Bloom works.
+        There are no ads, no affiliate marketing, no creepy tracking.
+        Just open technology for a fast, simple, and secure experience.`,
     },
     {
       question: 'Why do I need to pay to store my data while $BigCompany Drive is (almost) free?',
-      answer: `When you don't pay for a service, YOU are the product. It means that your personal data are either shared
+      answer: `When you don't pay for a service, YOU are the product. <br />
+      Your personal data are either shared
       with third party advertisers, or algortihms are applied to your data to sell you more and more products
-      you don't need. On the other side, on Bloom you data are encrypted and are not at the mercy of a tyranic government,
-      a rogue employee, or an over jealous conojoint.`,
+      you don't need, or even influence elections. <br />
+      On the other side, with Bloom, your data are encrypted and are not at the mercy of a tyranic government,
+      a rogue employee, or an over jealous conjoint.`,
     },
     {
-      question: 'I had a very poor experience with $BigCompany customer support, is yours better?',
-      answer: 'By being a human scale company, and we do our best to respond to any customer request as soon as we can.',
+      question: 'I had a very bad experience with $BigCompany customer support, is yours better?',
+      answer: `By being a human scale company, we do our best to provide the best and more ethical service as possible. Our clients
+      are the people paying us, not advertisers. <br />
+      Thus we do our best to respond to any customer request as soon and with the more love as we can.`,
     },
     {
       question: 'Do you issue refunds?',
       answer: `We do not issue refunds, but we offer a free tier
-      to allow you to test our applications.`,
+      to allow you to test our applications before subscribing.`,
     },
   ];
+
   // computed
   // lifecycle
   // watch
