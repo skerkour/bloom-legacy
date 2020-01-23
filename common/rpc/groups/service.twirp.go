1b3ecb3596002dea9c189af479e2297d6de9e4c4
z0mbie42 <sylvain@kerkour.fr>
2020-01-23 20:38:23 +0100

    common: Fix RemoveMembers rpc

--- common/rpc/groups/service.twirp.go
+++ common/rpc/groups/service.twirp.go
@@ -46,9 +46,9 @@ type Groups interface {
 	ListAllGroups(context.Context, *Empty) (*Empty, error)
 
 	// members related
-	RemoveMember(context.Context, *Empty) (*Empty, error)
+	RemoveMembers(context.Context, *Empty) (*Empty, error)
 
-	InviteMember(context.Context, *Empty) (*Empty, error)
+	InviteMembers(context.Context, *Empty) (*Empty, error)
 
 	AcceptInvitation(context.Context, *Empty) (*Empty, error)
 
@@ -88,8 +88,8 @@ func NewGroupsProtobufClient(addr string, client HTTPClient, opts ...twirp.Clien
 		prefix + "UpdateGroup",
 		prefix + "ListGroups",
 		prefix + "ListAllGroups",
-		prefix + "RemoveMember",
-		prefix + "InviteMember",
+		prefix + "RemoveMembers",
+		prefix + "InviteMembers",
 		prefix + "AcceptInvitation",
 		prefix + "DeclineInvitation",
 		prefix + "ResendInvitation",
@@ -203,10 +203,10 @@ func (c *groupsProtobufClient) ListAllGroups(ctx context.Context, in *Empty) (*E
 	return out, nil
 }
 
-func (c *groupsProtobufClient) RemoveMember(ctx context.Context, in *Empty) (*Empty, error) {
+func (c *groupsProtobufClient) RemoveMembers(ctx context.Context, in *Empty) (*Empty, error) {
 	ctx = ctxsetters.WithPackageName(ctx, "com.bloom42")
 	ctx = ctxsetters.WithServiceName(ctx, "Groups")
-	ctx = ctxsetters.WithMethodName(ctx, "RemoveMember")
+	ctx = ctxsetters.WithMethodName(ctx, "RemoveMembers")
 	out := new(Empty)
 	err := doProtobufRequest(ctx, c.client, c.opts.Hooks, c.urls[5], in, out)
 	if err != nil {
@@ -223,10 +223,10 @@ func (c *groupsProtobufClient) RemoveMember(ctx context.Context, in *Empty) (*Em
 	return out, nil
 }
 
-func (c *groupsProtobufClient) InviteMember(ctx context.Context, in *Empty) (*Empty, error) {
+func (c *groupsProtobufClient) InviteMembers(ctx context.Context, in *Empty) (*Empty, error) {
 	ctx = ctxsetters.WithPackageName(ctx, "com.bloom42")
 	ctx = ctxsetters.WithServiceName(ctx, "Groups")
-	ctx = ctxsetters.WithMethodName(ctx, "InviteMember")
+	ctx = ctxsetters.WithMethodName(ctx, "InviteMembers")
 	out := new(Empty)
 	err := doProtobufRequest(ctx, c.client, c.opts.Hooks, c.urls[6], in, out)
 	if err != nil {
@@ -352,8 +352,8 @@ func NewGroupsJSONClient(addr string, client HTTPClient, opts ...twirp.ClientOpt
 		prefix + "UpdateGroup",
 		prefix + "ListGroups",
 		prefix + "ListAllGroups",
-		prefix + "RemoveMember",
-		prefix + "InviteMember",
+		prefix + "RemoveMembers",
+		prefix + "InviteMembers",
 		prefix + "AcceptInvitation",
 		prefix + "DeclineInvitation",
 		prefix + "ResendInvitation",
@@ -467,10 +467,10 @@ func (c *groupsJSONClient) ListAllGroups(ctx context.Context, in *Empty) (*Empty
 	return out, nil
 }
 
-func (c *groupsJSONClient) RemoveMember(ctx context.Context, in *Empty) (*Empty, error) {
+func (c *groupsJSONClient) RemoveMembers(ctx context.Context, in *Empty) (*Empty, error) {
 	ctx = ctxsetters.WithPackageName(ctx, "com.bloom42")
 	ctx = ctxsetters.WithServiceName(ctx, "Groups")
-	ctx = ctxsetters.WithMethodName(ctx, "RemoveMember")
+	ctx = ctxsetters.WithMethodName(ctx, "RemoveMembers")
 	out := new(Empty)
 	err := doJSONRequest(ctx, c.client, c.opts.Hooks, c.urls[5], in, out)
 	if err != nil {
@@ -487,10 +487,10 @@ func (c *groupsJSONClient) RemoveMember(ctx context.Context, in *Empty) (*Empty,
 	return out, nil
 }
 
-func (c *groupsJSONClient) InviteMember(ctx context.Context, in *Empty) (*Empty, error) {
+func (c *groupsJSONClient) InviteMembers(ctx context.Context, in *Empty) (*Empty, error) {
 	ctx = ctxsetters.WithPackageName(ctx, "com.bloom42")
 	ctx = ctxsetters.WithServiceName(ctx, "Groups")
-	ctx = ctxsetters.WithMethodName(ctx, "InviteMember")
+	ctx = ctxsetters.WithMethodName(ctx, "InviteMembers")
 	out := new(Empty)
 	err := doJSONRequest(ctx, c.client, c.opts.Hooks, c.urls[6], in, out)
 	if err != nil {
@@ -650,11 +650,11 @@ func (s *groupsServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	case "/twirp/com.bloom42.Groups/ListAllGroups":
 		s.serveListAllGroups(ctx, resp, req)
 		return
-	case "/twirp/com.bloom42.Groups/RemoveMember":
-		s.serveRemoveMember(ctx, resp, req)
+	case "/twirp/com.bloom42.Groups/RemoveMembers":
+		s.serveRemoveMembers(ctx, resp, req)
 		return
-	case "/twirp/com.bloom42.Groups/InviteMember":
-		s.serveInviteMember(ctx, resp, req)
+	case "/twirp/com.bloom42.Groups/InviteMembers":
+		s.serveInviteMembers(ctx, resp, req)
 		return
 	case "/twirp/com.bloom42.Groups/AcceptInvitation":
 		s.serveAcceptInvitation(ctx, resp, req)
@@ -1321,7 +1321,7 @@ func (s *groupsServer) serveListAllGroupsProtobuf(ctx context.Context, resp http
 	callResponseSent(ctx, s.hooks)
 }
 
-func (s *groupsServer) serveRemoveMember(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+func (s *groupsServer) serveRemoveMembers(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 	header := req.Header.Get("Content-Type")
 	i := strings.Index(header, ";")
 	if i == -1 {
@@ -1329,9 +1329,9 @@ func (s *groupsServer) serveRemoveMember(ctx context.Context, resp http.Response
 	}
 	switch strings.TrimSpace(strings.ToLower(header[:i])) {
 	case "application/json":
-		s.serveRemoveMemberJSON(ctx, resp, req)
+		s.serveRemoveMembersJSON(ctx, resp, req)
 	case "application/protobuf":
-		s.serveRemoveMemberProtobuf(ctx, resp, req)
+		s.serveRemoveMembersProtobuf(ctx, resp, req)
 	default:
 		msg := fmt.Sprintf("unexpected Content-Type: %q", req.Header.Get("Content-Type"))
 		twerr := badRouteError(msg, req.Method, req.URL.Path)
@@ -1339,9 +1339,9 @@ func (s *groupsServer) serveRemoveMember(ctx context.Context, resp http.Response
 	}
 }
 
-func (s *groupsServer) serveRemoveMemberJSON(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+func (s *groupsServer) serveRemoveMembersJSON(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 	var err error
-	ctx = ctxsetters.WithMethodName(ctx, "RemoveMember")
+	ctx = ctxsetters.WithMethodName(ctx, "RemoveMembers")
 	ctx, err = callRequestRouted(ctx, s.hooks)
 	if err != nil {
 		s.writeError(ctx, resp, err)
@@ -1359,7 +1359,7 @@ func (s *groupsServer) serveRemoveMemberJSON(ctx context.Context, resp http.Resp
 	var respContent *Empty
 	func() {
 		defer ensurePanicResponses(ctx, resp, s.hooks)
-		respContent, err = s.Groups.RemoveMember(ctx, reqContent)
+		respContent, err = s.Groups.RemoveMembers(ctx, reqContent)
 	}()
 
 	if err != nil {
@@ -1367,7 +1367,7 @@ func (s *groupsServer) serveRemoveMemberJSON(ctx context.Context, resp http.Resp
 		return
 	}
 	if respContent == nil {
-		s.writeError(ctx, resp, twirp.InternalError("received a nil *Empty and nil error while calling RemoveMember. nil responses are not supported"))
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *Empty and nil error while calling RemoveMembers. nil responses are not supported"))
 		return
 	}
 
@@ -1394,9 +1394,9 @@ func (s *groupsServer) serveRemoveMemberJSON(ctx context.Context, resp http.Resp
 	callResponseSent(ctx, s.hooks)
 }
 
-func (s *groupsServer) serveRemoveMemberProtobuf(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+func (s *groupsServer) serveRemoveMembersProtobuf(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 	var err error
-	ctx = ctxsetters.WithMethodName(ctx, "RemoveMember")
+	ctx = ctxsetters.WithMethodName(ctx, "RemoveMembers")
 	ctx, err = callRequestRouted(ctx, s.hooks)
 	if err != nil {
 		s.writeError(ctx, resp, err)
@@ -1418,7 +1418,7 @@ func (s *groupsServer) serveRemoveMemberProtobuf(ctx context.Context, resp http.
 	var respContent *Empty
 	func() {
 		defer ensurePanicResponses(ctx, resp, s.hooks)
-		respContent, err = s.Groups.RemoveMember(ctx, reqContent)
+		respContent, err = s.Groups.RemoveMembers(ctx, reqContent)
 	}()
 
 	if err != nil {
@@ -1426,7 +1426,7 @@ func (s *groupsServer) serveRemoveMemberProtobuf(ctx context.Context, resp http.
 		return
 	}
 	if respContent == nil {
-		s.writeError(ctx, resp, twirp.InternalError("received a nil *Empty and nil error while calling RemoveMember. nil responses are not supported"))
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *Empty and nil error while calling RemoveMembers. nil responses are not supported"))
 		return
 	}
 
@@ -1450,7 +1450,7 @@ func (s *groupsServer) serveRemoveMemberProtobuf(ctx context.Context, resp http.
 	callResponseSent(ctx, s.hooks)
 }
 
-func (s *groupsServer) serveInviteMember(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+func (s *groupsServer) serveInviteMembers(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 	header := req.Header.Get("Content-Type")
 	i := strings.Index(header, ";")
 	if i == -1 {
@@ -1458,9 +1458,9 @@ func (s *groupsServer) serveInviteMember(ctx context.Context, resp http.Response
 	}
 	switch strings.TrimSpace(strings.ToLower(header[:i])) {
 	case "application/json":
-		s.serveInviteMemberJSON(ctx, resp, req)
+		s.serveInviteMembersJSON(ctx, resp, req)
 	case "application/protobuf":
-		s.serveInviteMemberProtobuf(ctx, resp, req)
+		s.serveInviteMembersProtobuf(ctx, resp, req)
 	default:
 		msg := fmt.Sprintf("unexpected Content-Type: %q", req.Header.Get("Content-Type"))
 		twerr := badRouteError(msg, req.Method, req.URL.Path)
@@ -1468,9 +1468,9 @@ func (s *groupsServer) serveInviteMember(ctx context.Context, resp http.Response
 	}
 }
 
-func (s *groupsServer) serveInviteMemberJSON(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+func (s *groupsServer) serveInviteMembersJSON(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 	var err error
-	ctx = ctxsetters.WithMethodName(ctx, "InviteMember")
+	ctx = ctxsetters.WithMethodName(ctx, "InviteMembers")
 	ctx, err = callRequestRouted(ctx, s.hooks)
 	if err != nil {
 		s.writeError(ctx, resp, err)
@@ -1488,7 +1488,7 @@ func (s *groupsServer) serveInviteMemberJSON(ctx context.Context, resp http.Resp
 	var respContent *Empty
 	func() {
 		defer ensurePanicResponses(ctx, resp, s.hooks)
-		respContent, err = s.Groups.InviteMember(ctx, reqContent)
+		respContent, err = s.Groups.InviteMembers(ctx, reqContent)
 	}()
 
 	if err != nil {
@@ -1496,7 +1496,7 @@ func (s *groupsServer) serveInviteMemberJSON(ctx context.Context, resp http.Resp
 		return
 	}
 	if respContent == nil {
-		s.writeError(ctx, resp, twirp.InternalError("received a nil *Empty and nil error while calling InviteMember. nil responses are not supported"))
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *Empty and nil error while calling InviteMembers. nil responses are not supported"))
 		return
 	}
 
@@ -1523,9 +1523,9 @@ func (s *groupsServer) serveInviteMemberJSON(ctx context.Context, resp http.Resp
 	callResponseSent(ctx, s.hooks)
 }
 
-func (s *groupsServer) serveInviteMemberProtobuf(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+func (s *groupsServer) serveInviteMembersProtobuf(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 	var err error
-	ctx = ctxsetters.WithMethodName(ctx, "InviteMember")
+	ctx = ctxsetters.WithMethodName(ctx, "InviteMembers")
 	ctx, err = callRequestRouted(ctx, s.hooks)
 	if err != nil {
 		s.writeError(ctx, resp, err)
@@ -1547,7 +1547,7 @@ func (s *groupsServer) serveInviteMemberProtobuf(ctx context.Context, resp http.
 	var respContent *Empty
 	func() {
 		defer ensurePanicResponses(ctx, resp, s.hooks)
-		respContent, err = s.Groups.InviteMember(ctx, reqContent)
+		respContent, err = s.Groups.InviteMembers(ctx, reqContent)
 	}()
 
 	if err != nil {
@@ -1555,7 +1555,7 @@ func (s *groupsServer) serveInviteMemberProtobuf(ctx context.Context, resp http.
 		return
 	}
 	if respContent == nil {
-		s.writeError(ctx, resp, twirp.InternalError("received a nil *Empty and nil error while calling InviteMember. nil responses are not supported"))
+		s.writeError(ctx, resp, twirp.InternalError("received a nil *Empty and nil error while calling InviteMembers. nil responses are not supported"))
 		return
 	}
 
@@ -2620,19 +2620,19 @@ func callClientError(ctx context.Context, h *twirp.ClientHooks, err twirp.Error)
 }
 
 var twirpFileDescriptor0 = []byte{
-	// 222 bytes of a gzipped FileDescriptorProto
+	// 219 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x9c, 0xd2, 0x31, 0x4b, 0x04, 0x31,
-	0x10, 0x05, 0xe0, 0x46, 0x57, 0x99, 0x53, 0x38, 0x53, 0x89, 0xa5, 0x3f, 0x20, 0x07, 0xe7, 0xa9,
-	0x70, 0x56, 0xe7, 0xad, 0x88, 0xa0, 0xcd, 0x82, 0x8d, 0xdd, 0x6e, 0xf6, 0x21, 0x81, 0x24, 0x13,
-	0x92, 0xb8, 0xe0, 0x8f, 0x17, 0x64, 0xa3, 0x85, 0x85, 0xc5, 0x66, 0xcb, 0x19, 0xde, 0xc7, 0x4c,
-	0xf1, 0xe8, 0x3c, 0x78, 0xb5, 0x7a, 0x0f, 0xfc, 0xe1, 0xe3, 0x2a, 0x22, 0x0c, 0x5a, 0x41, 0xfa,
-	0xc0, 0x89, 0xc5, 0x42, 0xb1, 0x95, 0x9d, 0x61, 0xb6, 0x9b, 0xf5, 0xe5, 0x11, 0x1d, 0x3e, 0x58,
-	0x9f, 0x3e, 0xd7, 0x5f, 0x07, 0x54, 0x3d, 0xe6, 0xb8, 0xb8, 0xa6, 0xc5, 0x3e, 0xa0, 0x4d, 0xc8,
-	0xb3, 0x10, 0xf2, 0x0f, 0x90, 0x39, 0x7d, 0xf1, 0xcf, 0x6e, 0x64, 0x35, 0x0c, 0x66, 0xb0, 0x57,
-	0xdf, 0x17, 0x5f, 0xdb, 0x10, 0x3d, 0xeb, 0x98, 0x7e, 0x5f, 0x9e, 0xaa, 0x6e, 0xe9, 0x74, 0x54,
-	0x3b, 0x63, 0x0a, 0xe1, 0x0d, 0x9d, 0x34, 0xb0, 0x3c, 0xe0, 0x05, 0xb6, 0x43, 0x28, 0x71, 0x4f,
-	0x6e, 0xd0, 0xa9, 0xd4, 0x6d, 0x69, 0xb9, 0x53, 0x0a, 0x3e, 0x65, 0xdd, 0x26, 0xcd, 0x6e, 0xb2,
-	0xbd, 0xa3, 0xb3, 0x1a, 0xca, 0x68, 0x87, 0x19, 0x78, 0x4b, 0xcb, 0x06, 0x11, 0xae, 0x9f, 0x67,
-	0xf7, 0xad, 0x53, 0x30, 0xe5, 0xf6, 0xfe, 0xf8, 0xad, 0xfa, 0x69, 0x6b, 0x57, 0xe5, 0x9a, 0x5e,
-	0x7d, 0x07, 0x00, 0x00, 0xff, 0xff, 0xa8, 0xba, 0x68, 0x21, 0xc2, 0x02, 0x00, 0x00,
+	0x10, 0x05, 0xe0, 0x46, 0x57, 0x99, 0x43, 0x38, 0x53, 0x89, 0xa5, 0x3f, 0x20, 0x07, 0xe7, 0x89,
+	0x70, 0x56, 0xe7, 0xae, 0x88, 0xa0, 0xcd, 0x82, 0x8d, 0xdd, 0x6e, 0xf6, 0x21, 0x81, 0x24, 0x13,
+	0x92, 0xb8, 0xe0, 0xaf, 0x57, 0x36, 0x5a, 0x58, 0x58, 0x6c, 0xb6, 0x9c, 0xe1, 0x7d, 0xcc, 0x14,
+	0x8f, 0x2e, 0x82, 0x57, 0x9b, 0xf7, 0xc0, 0x1f, 0x3e, 0x6e, 0x22, 0xc2, 0xa8, 0x15, 0xa4, 0x0f,
+	0x9c, 0x58, 0xac, 0x14, 0x5b, 0xd9, 0x1b, 0x66, 0xbb, 0xdb, 0x5e, 0x9d, 0xd0, 0xf1, 0x83, 0xf5,
+	0xe9, 0x73, 0xfb, 0x75, 0x44, 0xd5, 0x63, 0x8e, 0x8b, 0x1b, 0x5a, 0xd5, 0x01, 0x5d, 0x42, 0x9e,
+	0x85, 0x90, 0x7f, 0x80, 0xcc, 0xe9, 0xcb, 0x7f, 0x76, 0x13, 0x6b, 0x60, 0xb0, 0x80, 0xbd, 0xfa,
+	0xa1, 0xf8, 0xda, 0x8e, 0xe8, 0x59, 0xc7, 0xf4, 0xfb, 0xf2, 0x5c, 0x75, 0x4b, 0x67, 0x93, 0x3a,
+	0x18, 0x53, 0x0e, 0x5b, 0x58, 0x1e, 0xf1, 0x02, 0xdb, 0x23, 0x14, 0xc1, 0x27, 0x37, 0xea, 0x54,
+	0x0c, 0xf7, 0xb4, 0x3e, 0x28, 0x05, 0x9f, 0x32, 0xef, 0x92, 0x66, 0x37, 0xdb, 0xde, 0xd1, 0x79,
+	0x03, 0x65, 0xb4, 0xc3, 0x02, 0xbc, 0xa7, 0x75, 0x8b, 0x08, 0x37, 0x2c, 0xb3, 0x75, 0xe7, 0x14,
+	0x4c, 0xb9, 0xbd, 0x3f, 0x7d, 0xab, 0x7e, 0xfa, 0xda, 0x57, 0xb9, 0xa8, 0xd7, 0xdf, 0x01, 0x00,
+	0x00, 0xff, 0xff, 0x23, 0x8d, 0x2c, 0x1c, 0xc4, 0x02, 0x00, 0x00,
 }
