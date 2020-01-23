d7bf4a6bac8b70d2240c76c30ab85b5fdaec5034
z0mbie42 <sylvain@kerkour.fr>
2020-01-23 10:26:48 +0100

    faq

--- website/src/i18n/en.ts
+++ website/src/i18n/en.ts
@@ -140,8 +140,10 @@ export default {
       Your personal data are either shared
       with third party advertisers, or algorithms are applied to your data to sell you more and more products
       you don't need, or even influence elections. <br />
-      On the other side, with Bloom, your data are encrypted and are not at the mercy of a tyranic government,
-      a rogue employee, or an over jealous conjoint.`,
+      On the other side, with Bloom, we work for the people who value respect and privacy: You.
+      Not investors, not advertisers.`,
+      // On the other side, with Bloom, your data are encrypted and are not at the mercy of a tyranic government,
+      // a rogue employee, or an over jealous conjoint.`,
     q5: 'I had a very bad experience with $BigCompany customer support, is yours better?',
     a5: `Being an hulan scale company, we do our best to provide a fast and friendly support. <br />
       Our clients are the people paying us, not the advertisers.`,
