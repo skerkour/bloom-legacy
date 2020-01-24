df2806831bfdfe1c38bec813bfcf097c58dda43b
z0mbie42 <sylvain@kerkour.fr>
2020-01-24 14:18:25 +0100

    server: groups.DeleteGroup

--- server/domain/groups/consts.go
+++ server/domain/groups/consts.go
@@ -2,6 +2,7 @@ package groups
 
 const (
 	ErrorCreateGroupMsg = "Error creating new group. Please try again."
+	ErrorDeleteGroupMsg = "Error deleting group. Please try again."
 	RoleMember          = "member"
 	RoleAdministrator   = "administrator"
 )
--- server/domain/groups/delete_group.go
+++ server/domain/groups/delete_group.go
@@ -8,9 +8,24 @@ import (
 	"gitlab.com/bloom42/libs/rz-go"
 )
 
-func DeleteGroup(ctx context.Context, tx *sqlx.Tx, id string) twirp.Error {
+func DeleteGroup(ctx context.Context, tx *sqlx.Tx, groupId string) twirp.Error {
 	logger := rz.FromCtx(ctx)
-	_ = logger
+	var err error
+	// delete group
+	queryDeleteGroup := "DELETE FROM groups WHERE id = $1"
+	_, err = tx.Exec(queryDeleteGroup, groupId)
+	if err != nil {
+		logger.Error("groups.DeleteGroup: deleting group", rz.Err(err))
+		return twirp.InternalError(ErrorDeleteGroupMsg)
+	}
+
+	// delete members
+	queryDeleteGroupMembers := "DELETE FROM groups_members WHERE group_id = $1"
+	_, err = tx.Exec(queryDeleteGroupMembers, groupId)
+	if err != nil {
+		logger.Error("groups.DeleteGroup: deleteing members", rz.Err(err))
+		return twirp.InternalError(ErrorDeleteGroupMsg)
+	}
 
 	return nil
 }
