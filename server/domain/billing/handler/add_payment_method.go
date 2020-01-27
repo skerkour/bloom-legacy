c502e798d1d69f7947bc1314da3575506bcfcb82
z0mbie42 <sylvain@kerkour.fr>
2020-01-27 11:33:17 +0100

    server: implement billing.rpc

---
+++ server/domain/billing/handler/add_payment_method.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/twitchtv/twirp"
+	rpc "gitlab.com/bloom42/bloom/common/rpc/billing"
+	"gitlab.com/bloom42/bloom/server/api/apictx"
+	"gitlab.com/bloom42/libs/rz-go"
+)
+
+func (s Handler) AddPaymentMethod(ctx context.Context, params *rpc.AddPaymentMethodParams) (*rpc.PaymentMethod, error) {
+	ret := &rpc.PaymentMethod{}
+	logger := rz.FromCtx(ctx)
+	apiCtx, ok := ctx.Value(apictx.Key).(*apictx.Context)
+	if !ok {
+		return ret, twirp.InternalError("internal error")
+	}
+	if apiCtx.AuthenticatedUser == nil {
+		twerr := twirp.NewError(twirp.Unauthenticated, "authentication required")
+		return ret, twerr
+	}
+
+	_ = logger
+
+	return ret, nil
+}
