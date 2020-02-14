d1bb064b92c340f91e33b26bb83458607e272420
z0mbie42 <sylvain@kerkour.fr>
2020-02-14 09:38:09 +0100

    server: query.users

---
+++ server/domain/users/find_all_users.go
@@ -0,0 +1,23 @@
+package users
+
+import (
+	"context"
+
+	"gitlab.com/bloom42/bloom/server/db"
+	"gitlab.com/bloom42/libs/rz-go"
+)
+
+func FindAllUsers(ctx context.Context) ([]User, error) {
+	ret := []User{}
+	var err error
+	logger := rz.FromCtx(ctx)
+
+	queryFind := "SELECT * FROM users"
+	err = db.DB.Get(ret, queryFind)
+	if err != nil {
+		logger.Error("users.FindAllUsers: finding users", rz.Err(err))
+		return ret, NewError(ErrorUserNotFound)
+	}
+
+	return ret, err
+}
