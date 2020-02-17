d1b5887c9ea1a1ff4ea3dc1059cb74207f9d2f93
z0mbie42 <sylvain@kerkour.fr>
2020-02-17 15:46:28 +0100

    core: Update api models

--- core/api/model/group.go
+++ core/api/model/group.go
@@ -9,4 +9,5 @@ type Group struct {
 	CreatedAt   *time.Time `json:"createdAt"`
 	Name        string     `json:"name"`
 	Description string     `json:"description"`
+	AvatarURL   *string    `json:"avatarUrl"`
 }
