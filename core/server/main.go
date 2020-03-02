c7ec92a58f426fa25dd77719dc5faa1dc5164914
z0mbie42 <sylvain@kerkour.fr>
2020-03-02 15:17:50 +0100

    core: remove cors from server...

--- core/server/main.go
+++ core/server/main.go
@@ -13,11 +13,6 @@ import (
 func handleElectronPost(w http.ResponseWriter, r *http.Request) {
 	var messageIn core.MessageIn
 
-	setupResponse(&w, r)
-	if (*r).Method == "OPTIONS" {
-		return
-	}
-
 	// Try to decode the request body into the struct. If there is an error,
 	// respond to the client with the error message and a 400 status code.
 	err := json.NewDecoder(r.Body).Decode(&messageIn)
@@ -35,16 +30,6 @@ func handleElectronPost(w http.ResponseWriter, r *http.Request) {
 	// Do something with the Person struct...
 	w.Header().Set("content-type", "application/json")
 	w.Write(data)
-}
-
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-}
-
-func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func main() {
