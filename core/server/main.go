4e13dc63f68545005db8a385162b3667c499dd5c
Sylvain Kerkour <sylvain@kerkour.fr>
2020-03-09 17:15:31 +0100

    desktop: delete unix socket on server shutdown. close #224

--- core/server/main.go
+++ core/server/main.go
@@ -4,11 +4,16 @@ import (
 	"encoding/json"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"gitlab.com/bloom42/bloom/core"
 	"gitlab.com/bloom42/libs/rz-go"
 	"gitlab.com/bloom42/libs/rz-go/log"
 )
+
+const UNIX_SOCKET = "/tmp/com.bloom42.bloom.sock"
 
 func handleElectronPost(w http.ResponseWriter, r *http.Request) {
 	var messageIn core.MessageIn
@@ -17,7 +22,6 @@ func handleElectronPost(w http.ResponseWriter, r *http.Request) {
 	if (*r).Method == "OPTIONS" {
 		return
 	}
-
 
 	// Try to decode the request body into the struct. If there is an error,
 	// respond to the client with the error message and a 400 status code.
@@ -44,11 +48,29 @@ func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
-
 func main() {
+	// remove Unix socket if exists
+	os.Remove(UNIX_SOCKET)
+
+	// handle SIGKILL
+	signalCatcher := make(chan os.Signal, 2)
+	signal.Notify(signalCatcher, os.Interrupt,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go func() {
+		<-signalCatcher
+		err := os.Remove(UNIX_SOCKET)
+		if err != nil {
+			log.Fatal("error removing unix socket", rz.Err(err))
+		}
+		os.Exit(0)
+	}()
+
 	http.HandleFunc("/electronCall", handleElectronPost)
 
-	unixListener, err := net.Listen("unix", "/tmp/com.bloom42.bloom.sock")
+	unixListener, err := net.Listen("unix", UNIX_SOCKET)
 	if err != nil {
 		log.Fatal("error listening to unix socket", rz.Err(err))
 	}
