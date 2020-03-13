ec35bc4b4da5b542749661f31535c61dae554461
z0mbie42 <sylvain@kerkour.fr>
2020-03-13 15:23:21 +0100

    core: groups.RemoveMembers

--- core/domain/groups/remove_members.go
+++ core/domain/groups/remove_members.go
@@ -1,1 +1,40 @@
 package groups
+
+import (
+	"context"
+
+	"gitlab.com/bloom42/bloom/core/api"
+	"gitlab.com/bloom42/bloom/core/api/model"
+	"gitlab.com/bloom42/libs/graphql-go"
+)
+
+func RemoveMembers(input model.RemoveGroupMembersInput) (*model.Group, error) {
+	client := api.Client()
+
+	var resp struct {
+		Group *model.Group `json:"removeGroupMembers"`
+	}
+	req := graphql.NewRequest(`
+	mutation($input: RemoveGroupMembersInput!) {
+		removeGroupMembers(input: $input) {
+			members {
+				edges {
+					role
+					joinedAt
+					node {
+						avatarUrl
+						username
+						displayName
+					}
+				}
+				totalCount
+			}
+		}
+	}
+	`)
+	req.Var("input", input)
+
+	err := client.Do(context.Background(), req, &resp)
+
+	return resp.Group, err
+}
--- core/groups.go
+++ core/groups.go
@@ -61,6 +61,17 @@ func handleGroupsMethod(method string, jsonParams json.RawMessage) MessageOut {
 			return InternalError(err) // TODO(z0mbie42): return error
 		}
 		return MessageOut{Data: res}
+	case "removeMembers":
+		var params model.RemoveGroupMembersInput
+		err := json.Unmarshal(jsonParams, &params)
+		if err != nil {
+			return InternalError(err) // TODO(z0mbie42): return error
+		}
+		res, err := groups.RemoveMembers(params)
+		if err != nil {
+			return InternalError(err) // TODO(z0mbie42): return error
+		}
+		return MessageOut{Data: res}
 	default:
 		return methodNotFoundError(method, "groups")
 	}
