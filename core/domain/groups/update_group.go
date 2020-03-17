86a40f28a50e5ec6629a600ed41375a4d3862af6
z0mbie42 <sylvain@kerkour.fr>
2020-03-17 11:17:48 +0100

    core: groups.UpdateGroup close #243

--- core/domain/groups/update_group.go
+++ core/domain/groups/update_group.go
@@ -1,1 +1,42 @@
 package groups
+
+import (
+	"context"
+
+	"gitlab.com/bloom42/bloom/core/api"
+	"gitlab.com/bloom42/bloom/core/api/model"
+	"gitlab.com/bloom42/bloom/core/db"
+	"gitlab.com/bloom42/libs/graphql-go"
+)
+
+func UpdateGroup(input model.GroupInput) (model.Group, error) {
+	client := api.Client()
+
+	var resp struct {
+		Group model.Group `json:"updateGroup"`
+	}
+	req := graphql.NewRequest(`
+	mutation ($input: GroupInput!) {
+		updateGroup(input: $input) {
+			id
+			createdAt
+			avatarUrl
+			name
+			description
+			members {
+				totalCount
+			}
+		}
+	}
+	`)
+	req.Var("input", input)
+
+	err := client.Do(context.Background(), req, &resp)
+	if err == nil {
+		group := resp.Group
+		_, err = db.DB.Exec("UPDATE groups SET name = ?, description = ?, avatar_url = ? WHERE id = ?",
+			group.Name, group.Description, group.AvatarURL, group.ID)
+	}
+
+	return resp.Group, err
+}
