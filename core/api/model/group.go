84fde35055da7331696e724257c1c7431f6964ef
z0mbie42 <sylvain@kerkour.fr>
2020-03-17 14:46:41 +0100

    core: to new schema

--- core/api/model/group.go
+++ core/api/model/group.go
@@ -5,11 +5,14 @@ import (
 )
 
 type Group struct {
-	ID          *string                    `json:"id"`
-	CreatedAt   *time.Time                 `json:"createdAt"`
-	Name        string                     `json:"name"`
-	Description string                     `json:"description"`
-	AvatarURL   *string                    `json:"avatarUrl"`
-	Members     *GroupMemberConnection     `json:"members"`
-	Invitations *GroupInvitationConnection `json:"invitations"`
+	ID             *string                    `json:"id"`
+	CreatedAt      *time.Time                 `json:"createdAt"`
+	Name           string                     `json:"name"`
+	Description    string                     `json:"description"`
+	AvatarURL      *string                    `json:"avatarUrl"`
+	Members        *GroupMemberConnection     `json:"members"`
+	Invitations    *GroupInvitationConnection `json:"invitations"`
+	Subscription   *BillingSubscription       `json:"subscription"`
+	Invoices       *InvoiceConnection         `json:"invoices"`
+	PaymentMethods *PaymentMethodConnection   `json:"paymentMethods"`
 }
