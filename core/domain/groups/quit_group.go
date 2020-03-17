555418192394ad80b47837e59e7896204e9a724d
z0mbie42 <sylvain@kerkour.fr>
2020-03-17 11:14:23 +0100

    core: groups.QuitGroup

--- core/domain/groups/quit_group.go
+++ core/domain/groups/quit_group.go
@@ -1,1 +1,31 @@
 package groups
+
+import (
+	"context"
+
+	"gitlab.com/bloom42/bloom/core/api"
+	"gitlab.com/bloom42/bloom/core/api/model"
+	"gitlab.com/bloom42/bloom/core/db"
+	"gitlab.com/bloom42/libs/graphql-go"
+)
+
+func QuitGroup(input model.QuitGroupInput) (bool, error) {
+	client := api.Client()
+
+	var resp struct {
+		Success bool `json:"quitGroup"`
+	}
+	req := graphql.NewRequest(`
+	mutation($input: QuitGroupInput!) {
+		quitGroup(input: $input)
+	}
+	`)
+	req.Var("input", input)
+
+	err := client.Do(context.Background(), req, &resp)
+	if err == nil {
+		_, err = db.DB.Exec("DELETE FROM groups WHERE id = ?", input.ID)
+	}
+
+	return resp.Success, err
+}
