14f9bcce14a002c913ecf89632d33f4c48980338
z0mbie42 <sylvain@kerkour.fr>
2020-03-17 11:12:34 +0100

    core: groups.AcceptInvitation persists group

--- core/domain/groups/accept_invitation.go
+++ core/domain/groups/accept_invitation.go
@@ -5,23 +5,38 @@ import (
 
 	"gitlab.com/bloom42/bloom/core/api"
 	"gitlab.com/bloom42/bloom/core/api/model"
+	"gitlab.com/bloom42/bloom/core/db"
 	"gitlab.com/bloom42/libs/graphql-go"
 )
 
-func AcceptInvitation(input model.AcceptGroupInvitationInput) (bool, error) {
+func AcceptInvitation(input model.AcceptGroupInvitationInput) (*model.Group, error) {
 	client := api.Client()
 
 	var resp struct {
-		Success bool `json:"acceptGroupInvitation"`
+		Group *model.Group `json:"acceptGroupInvitation"`
 	}
 	req := graphql.NewRequest(`
 	mutation($input: AcceptGroupInvitationInput!) {
-		acceptGroupInvitation(input: $input)
+		acceptGroupInvitation(input: $input) {
+			id
+			createdAt
+			avatarUrl
+			name
+			description
+			members {
+				totalCount
+			}
+		}
 	}
 	`)
 	req.Var("input", input)
 
 	err := client.Do(context.Background(), req, &resp)
+	if err == nil {
+		group := resp.Group
+		_, err = db.DB.Exec(`INSERT INTO groups (id, created_at, name, description, avatar_url)
+			VALUES (?, ?, ?, ?, ?)`, group.ID, group.CreatedAt, group.Name, group.Description, group.AvatarURL)
+	}
 
-	return resp.Success, err
+	return resp.Group, err
 }
