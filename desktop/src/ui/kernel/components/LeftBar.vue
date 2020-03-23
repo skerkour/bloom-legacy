adce73e56aed44a8fb876b2292a1d1b1766c1fcc
z0mbie42 <sylvain@kerkour.fr>
2020-03-23 23:47:05 +0100

    desktop: improve left bar

--- desktop/src/ui/kernel/components/LeftBar.vue
+++ desktop/src/ui/kernel/components/LeftBar.vue
@@ -10,81 +10,47 @@
   >
     <v-list>
       <v-list-item>
-        <v-menu
-          v-model="accountMenu"
-          :close-on-content-click="false"
-          :nudge-width="200"
-        >
-          <template v-slot:activator="{ on }">
-            <v-list-item-avatar color="white" class="blm-pointer" v-on="on">
-              <v-img :src="$store.state.me.avatarUrl" v-if="$store.state.me.avatarUrl"/>
-              <v-icon medium color="grey" v-else>mdi-account</v-icon>
-            </v-list-item-avatar>
-
-            <!-- <v-list-item-title>John Leider</v-list-item-title> -->
-          </template>
-
-          <v-card>
-            <v-list>
-              <v-list-item>
-                <v-list-item-avatar>
-                  <v-img :src="avatarUrl" alt="Profile Picture" />
-                </v-list-item-avatar>
-
-                <v-list-item-content class="text-left">
-                  <v-list-item-title>{{ $store.state.me.displayName }}</v-list-item-title>
-                  <v-list-item-subtitle>@{{ $store.state.me.username }}</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-
-              <v-list-item class="justify-center">
-                <v-btn
-                  text
-                  to="/myaccount"
-                  @click="accountMenu = false"
-                >
-                  <v-icon left>mdi-account</v-icon> My Account
-                </v-btn>
-
-                <v-btn
-                  text
-                  to="/preferences"
-                  @click="accountMenu = false"
-                >
-                  <v-icon left>mdi-settings</v-icon> Preferences
-                </v-btn>
-              </v-list-item>
-            </v-list>
-
-          </v-card>
-        </v-menu>
-      </v-list-item>
-
-      <v-list-item>
-        <v-list-item-avatar
-          color="white"
-          class="blm-pointer"
-          @click="goto('/groups')"
-        >
-          <v-icon
-            medium
-            color="grey"
-          >mdi-account-group</v-icon>
+        <v-list-item-avatar color="white" class="blm-pointer" @click="goto('/myaccount')">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-img :src="$store.state.me.avatarUrl" v-if="$store.state.me.avatarUrl" v-on="on"/>
+              <v-icon medium color="grey" v-else v-on="on">mdi-account</v-icon>
+            </template>
+            <span>My Account</span>
+          </v-tooltip>
         </v-list-item-avatar>
-
-        <!-- <v-list-item-title>John Leider</v-list-item-title> -->
       </v-list-item>
 
       <v-list-item>
-        <v-list-item-avatar
-          color="white"
-          class="blm-pointer"
-          @click="openAllAppsDialog"
-        >
-          <v-icon
-            medium
-            color="grey"
-          >mdi-apps</v-icon>
+        <v-list-item-avatar color="white" class="blm-pointer" @click="goto('/preferences')">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+               <v-icon medium color="grey" v-on="on">mdi-settings</v-icon>
+            </template>
+            <span>Preferences</span>
+          </v-tooltip>
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-avatar color="white" class="blm-pointer" @click="goto('/groups')">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+               <v-icon medium color="grey" v-on="on">mdi-account-group</v-icon>
+            </template>
+            <span>Groups</span>
+          </v-tooltip>
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-avatar color="white" class="blm-pointer" @click="openAllAppsDialog">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+               <v-icon medium color="grey" v-on="on">mdi-apps</v-icon>
+            </template>
+            <span>All apps</span>
+          </v-tooltip>
         </v-list-item-avatar>
       </v-list-item>
 
