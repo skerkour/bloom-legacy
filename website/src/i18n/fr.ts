2994e998a31c40b88020e933bb88f926d412912b
z0mbie42 <sylvain@kerkour.fr>
2020-03-25 16:04:49 +0100

    website: i18n fr #203

--- website/src/i18n/fr.ts
+++ website/src/i18n/fr.ts
@@ -3,6 +3,10 @@ export default {
     other_downloads: 'Tous les Téléchargements',
     learn_more: 'Plus d\'info',
     coming_soon_btn: 'Sortie début 2020',
+    pain1: 'You are done with your privacy invaded by advertisers?',
+    pain2: "Fatigué d'être abusé par les grosses compagnies?",
+    pain3: 'Vous avez perdu toute confiance dans les GAFAM?',
+    pain4: 'La vie privée n\'est pas morte!',
   },
   appbar: {
     download: 'Télécharger',
@@ -13,8 +17,7 @@ export default {
     subtitle2: '<b>Reprenez le contrôle avec <span class="blm-rounded-elegance"><b>Bloom</b></span></b>',
     explore_features: 'Découvrez les fonctionnalités',
     feature1: {
-      // title: 'Fast, Simple, Secure',
-      title: 'Un lieu sûr pour toutes vos données',
+      title: 'Rapide, Simple, Sécure',
       description: `En tant que projet Open Source, n'importe qui peut regarder comment fonctionne
       Bloom. Nous utilisons l'état de l'art en cryptographie pour garder vos données en sécurité.
       Il n'y a pas de publicité, pas de marketing d'affiliation, pas de trackers envahissants. <br />
@@ -27,6 +30,13 @@ export default {
       Enfin des outils pensés pour passer moins de temps devant un écran, et plus dans la vraie vie.<br />
       N'arrêtez jamais le fun.`,
     },
+    feature3: {
+      title: 'Synchronisé avec tous vos appareils',
+      description: `Il est simple d'emmener son contenu partout. Commencez sur votre ordinateur
+        puis éditez sur votre smartphone. Bloom fonctionne sur n'importe quel appareil ou plateforme. Même la ligne de commande!`,
+    },
+    product_hunt: '#1 Produit du jour',
+    hacker_news: '#1 sur',
   },
   p404: {
     not_found: 'Page non trouvée',
