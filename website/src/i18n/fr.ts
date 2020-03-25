23f686118bfe8a3114745fcf30d70088205e8059
z0mbie42 <sylvain@kerkour.fr>
2020-03-25 18:11:54 +0100

    website: i18n fr #203

--- website/src/i18n/fr.ts
+++ website/src/i18n/fr.ts
@@ -150,4 +150,159 @@ export default {
     month: 'mois',
     others: 'Autres',
   },
+  faq: {
+    title: 'Frequently Asked Questions',
+    q1: 'How secure are my data?',
+    a1: `Some of your most personal moments are shared on WhatsApp, which is why we built end-to-end encryption.
+      We use state of the art cryptography
+      (<a href="https://tools.ietf.org/html/draft-irtf-cfrg-xchacha-03" target="_blank" rel="noopener noreferer">XChaCha20Poly1305</a>,
+      <a href="https://en.wikipedia.org/wiki/Argon2" target="_blank" rel="noopener noreferer">Argon2id</a>,
+      <a href="https://en.wikipedia.org/wiki/BLAKE_(hash_function)" target="_blank" rel="noopener noreferer">Blake2b</a>)
+      to securely store your all your data.
+      Your data are encrypted on your own devices and (even if we wanted) we can't read them.
+      You can learn more about our cryptographic architecture on
+      <a href="https://gitlab.com/bloom42/bloom/-/wikis/cryptography" target="_blank" rel="noopener noreferer">the wiki</a>.<br />
+      Also, we do not store financial data ourselves, we use  <a href="https://stripe.com" target="_blank" rel="noopener noreferer">Stripe</a>.`,
+    q2: 'Where the encrypted data are stored?',
+    a2: `The <a href="https://en.wikipedia.org/wiki/End-to-end_encryption" target="_blank" rel="noopener noreferer">end-to-end</a>
+      encrypted data are stored in a  <a href="scaleway.com" target="_blank" rel="noopener noreferer">Scaleway</a>
+      datacenter in a privacy friendly country: France.`,
+    q3: 'Why Bloom is more ethical than other service providers?',
+    a3: `Bloom is <b>100% Open Source</b>. Anyone can inspect how Bloom works.
+      Also, there are no ads, no affiliate marketing, no creepy tracking.
+      Just open technology for a fast, simple, and secure experience.`,
+    q4: 'Why do I need to pay to store my data while $BigCompany Drive is (almost) free?',
+    a4: `When you don't pay for a service, YOU are the product. <br />
+      Your personal data are either shared
+      with third party advertisers, or algorithms are applied to your data to sell you more and more products
+      you don't need, or even influence elections. <br />
+      On the other side, with Bloom, we work for the people who value respect and privacy: You.
+      Not investors, not advertisers.`,
+    q5: 'I had a very bad experience with $BigCompany customer support, is yours better?',
+    a5: `Being an hulan scale company, we do our best to provide a fast and friendly support. <br />
+      The people we value are our customers paying us, not the advertisers.`,
+    q6: 'Do you issue refunds?',
+    a6: `We do not issue refunds, but we offer a free tier
+        to allow you to test our applications before subscribing.`,
+    q7: 'What happens if I forgot my password and all my devices are signed out?',
+    a7: `Due to how encryption works, unfortunately, if you lose your password and all your devices are logged out, you lose all your data. <br />
+    If you forget your password but at least one of your devices is signed in, you can normally change your password, without losing nothing. <br />
+    We recommend you to note your account's password on a sheet of paper and to hide it somewhere.`,
+    q8: 'Why Bloom is an application and not available through web browsers?',
+    a8: `In the beginning Bloom was available through web browsers. But after some time we were not satisfied with the poor experience it provided
+    and the tradeoffs we had to make. This is why we decided to switch to native applications only. It allows
+    offline support, strong encryption, better performance. We want Bloom to be as resilient as possible and the web does not provide
+    this feature.`,
+  },
+  features: {
+    title: 'A safe place for all your data',
+    solution: 'That\'s why we built Bloom',
+    files_description: `A safe place for all your files. Bloom Files is end to end encrypted.<br />
+    <b>1GB free.</b>`,
+    files: {
+      description: 'A safe place for all your files',
+      feature1: {
+        name: 'Store any file',
+        description: `Keep photos, stories, designs, drawings, recordings, videos, and more.
+        Your first <b>1GB</b> of storage are free.`,
+      },
+      feature2: {
+        name: 'See your stuff anywhere',
+        description: `Your files in Files can be reached from any smartphone, tablet, or computer.
+        So wherever you go, your files follow.`,
+      },
+      feature3: {
+        name: 'Keep your files safe',
+        description: `If something happens to your device, you don't have to worry about losing
+          your files or photos – they're in your Bloom Files.`,
+      },
+    },
+    calendar_description: 'Get organized on the go with the Bloom Calendar!',
+    calendar: {
+      description: 'Make the most of every day',
+      feature1: {
+        name: 'Easy to use',
+        description: `Make the most of every day with a simple to use interface, designed to adapt to
+        everyone's needs.`,
+      },
+      feature2: {
+        name: 'All your calendars in one place',
+        description: `Share calendars with friends and coworkers and access them all from the same
+        unified interface`,
+      },
+    },
+    notes_description: `Quickly capture what’s on your mind and get a reminder later
+    at the right place or time.`,
+    notes: {
+      description: 'Quickly capture what’s on your mind.',
+      feature1: {
+        name: 'Feel organized without the effort',
+        description: `Bloom Notes helps you capture and prioritize ideas, projects,
+        so nothing falls through the cracks.`,
+      },
+      feature2: {
+        name: 'Access from anywhere',
+        description: `Take notes. It's easy to pull up your content from anywhere. Start on your
+        laptop then update notes on your phone. Bloom Notes works on any device or platform.`,
+      },
+    },
+    bitflow_description: `Your cloud-based download manager.
+    Download any file and torrent directly to your Bloom files.`,
+    bitflow: {
+      description: 'Download torrents to a secure cloud. Fast. Online.',
+      feature1: {
+        name: 'Download anywhere, anytime',
+        description: `Your files are downloaded directly to your Bloom files and can can be accessed from
+        any smartphone, tablet, or computer. So wherever you go, your downloaded files follow.`,
+      },
+      feature2: {
+        name: 'No need to install a torrent client',
+        description: `With Bitflow you don’t need to download any program, all you need is a
+        browser. Bitflow currently supports Torrent and HTTP(S) protocols.`,
+      },
+      feature3: {
+        name: 'Download big files faster',
+        description: `It takes a long time to download large files with a residential bandwidth.
+        Not with Bitflow! We'll download your files in the blink of an eye!`,
+      },
+    },
+    arcade_description: 'Never stop having fun!',
+    arcade: {
+      description: 'The best place to play',
+      feature1: {
+        name: 'Play from any device',
+        description: `Gaming should not be frustrating because the lack of a specific device. Bloom
+        Arcade's games can run on any platform with a web browser`,
+      },
+      feature2: {
+        name: 'Committed to your privacy',
+        description: 'None of our games contains ad.',
+      },
+    },
+    contacts_description: 'Keep your contacts organized and up to date.',
+    contacts: {
+      description: 'Keep your contacts organized and up to date',
+      feature1: {
+        name: 'Back up your contacts and sync them across all your devices',
+        description: `Safely back up your contacts to the cloud
+        to access from any device.`,
+      },
+      feature2: {
+        name: 'Keep your contacts organized and up to date',
+        description: `Easily add contacts and edit information like phone numbers, emails,
+        and profiles.`,
+      },
+    },
+    qrcodes_description: 'Bloom apps are the perfect tool to scan your QR codes anytime and anywhere.',
+  },
+  security: {
+    title: 'Security',
+    description: `Keeping our customer data safe and secure is our top priority. We take threats very
+    seriously and work hard to protect our customers and their data. Your input and feedback
+      on our security is greatly appreciated.`,
+    issues_title: 'Reporting Issues',
+    issues_description: `Send reports directly to <a href="mailto:hello@bloom.sh">hello@bloom.sh</a>
+      and we will get back to you faster than the light.`,
+    hall_title: 'Hall of fame',
+  },
 };
