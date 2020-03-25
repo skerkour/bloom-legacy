0c84e80e28d0951500987937ea7ec4f257773482
z0mbie42 <sylvain@kerkour.fr>
2020-03-25 15:57:15 +0100

    remove roboto

--- website/src/i18n/index.ts
+++ website/src/i18n/index.ts
@@ -4,16 +4,12 @@ import { getLang } from '@/utils/lang';
 
 import en from './en';
 import fr from './fr';
-// import es from './es';
-// import pt from './pt';
 
 Vue.use(VueI18n);
 
 const messages = {
   en,
-  // es,
   fr,
-  // pt,
 };
 
 // Create VueI18n instance with options
