dfce60c1403ba8887629cf9329012d66548a6fd5
z0mbie42 <sylvain@kerkour.fr>
2020-04-08 15:15:45 +0200

    server: tls 1.3

--- bloom/server/api/run.go
+++ bloom/server/api/run.go
@@ -98,6 +98,13 @@ func Run() error {
 		}
 		tlsConfig = &tls.Config{
 			GetCertificate: certManager.GetCertificate,
+			CipherSuites: []uint16{
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+			},
+			MinVersion:               tls.VersionTLS13,
+			PreferServerCipherSuites: true,
 		}
 	}
 
