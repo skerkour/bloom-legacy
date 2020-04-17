c5c7f605ecf8b3c38962e681341464d4736ce854
z0mbie42 <sylvain@kerkour.fr>
2020-04-17 09:24:14 +0200

    server: Update TLS config and SERVER_IDLE_TIMEOUT

--- cmd/bloom/server/api/consts.go
+++ cmd/bloom/server/api/consts.go
@@ -8,5 +8,5 @@ const (
 	HeaderKeyBloomRequestID = "X-Bloom-Request-ID"
 	SERVER_READ_TIMEOUT     = 5 * time.Second
 	SERVER_WRITE_TIMEOUT    = 10 * time.Second
-	SERVER_IDLE_TIMEOUT     = 15 * time.Second
+	SERVER_IDLE_TIMEOUT     = 30 * time.Second
 )
--- cmd/bloom/server/api/run.go
+++ cmd/bloom/server/api/run.go
@@ -98,18 +98,20 @@ func Run() error {
 		}
 		tlsConfig = &tls.Config{
 			GetCertificate: certManager.GetCertificate,
+			// Only use curves which have assembly implementations
+			CurvePreferences: []tls.CurveID{
+				tls.CurveP256,
+				tls.X25519, // Go 1.8 only
+			},
 			CipherSuites: []uint16{
-				tls.TLS_AES_128_GCM_SHA256,
-				tls.TLS_AES_256_GCM_SHA384,
-				tls.TLS_CHACHA20_POLY1305_SHA256,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			},
-			MinVersion:               tls.VersionTLS13,
+			MinVersion:               tls.VersionTLS12,
 			PreferServerCipherSuites: true,
 		}
 	}
