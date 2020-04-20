3a8d1c2075e0d3eb576d6c95c931ebf99140f892
z0mbie42 <sylvain@kerkour.fr>
2020-04-20 23:11:33 +0200

    server: Update TLS config

--- cmd/bloom/server/api/run.go
+++ cmd/bloom/server/api/run.go
@@ -100,18 +100,13 @@ func Run() error {
 			GetCertificate: certManager.GetCertificate,
 			// Only use curves which have assembly implementations
 			CurvePreferences: []tls.CurveID{
-				tls.CurveP256,
-				tls.X25519, // Go 1.8 only
+				tls.X25519,
 			},
 			CipherSuites: []uint16{
 				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			},
-			MinVersion:               tls.VersionTLS12,
+			MinVersion:               tls.VersionTLS13,
 			PreferServerCipherSuites: true,
 		}
 	}
