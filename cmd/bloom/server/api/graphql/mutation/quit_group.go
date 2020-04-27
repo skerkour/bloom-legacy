75713cb85b38caf73278adcfebc902d4e924bac0
z0mbie42 <sylvain@kerkour.fr>
2020-04-27 11:13:23 +0200

    server: groups.QuitGroup to domain

--- cmd/bloom/server/api/graphql/mutation/quit_group.go
+++ cmd/bloom/server/api/graphql/mutation/quit_group.go
@@ -6,49 +6,23 @@ import (
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/api/apiutil"
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/api/graphql/gqlerrors"
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/api/graphql/model"
-	"gitlab.com/bloom42/bloom/cmd/bloom/server/db"
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/domain/groups"
-	"gitlab.com/bloom42/lily/rz"
 )
 
-func (r *Resolver) QuitGroup(ctx context.Context, input model.QuitGroupInput) (bool, error) {
-	ret := false
-	logger := rz.FromCtx(ctx)
+// QuitGroup quit a group
+func (r *Resolver) QuitGroup(ctx context.Context, input model.QuitGroupInput) (ret bool, err error) {
 	currentUser := apiutil.UserFromCtx(ctx)
 
 	if currentUser == nil {
 		return ret, gqlerrors.AuthenticationRequired()
 	}
 
-	tx, err := db.DB.Beginx()
+	err = groups.QuitGroup(ctx, currentUser, input.ID)
 	if err != nil {
-		logger.Error("mutation.QuitGroup: Starting transaction", rz.Err(err))
-		return ret, gqlerrors.New(groups.NewError(groups.ErrorQuittingGroup))
+		err = gqlerrors.New(err)
+		return
 	}
 
-	var group groups.Group
-
-	queryGetGroup := "SELECT * FROM groups WHERE id = $1"
-	err = tx.Get(&group, queryGetGroup, input.ID)
-	if err != nil {
-		tx.Rollback()
-		logger.Error("mutation.QuitGroup: fetching group", rz.Err(err),
-			rz.String("group.id", input.ID.String()))
-		return ret, gqlerrors.New(groups.NewError(groups.ErrorGroupNotFound))
-	}
-
-	err = groups.QuitGroup(ctx, tx, *currentUser, group)
-	if err != nil {
-		tx.Rollback()
-		return ret, gqlerrors.New(err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		tx.Rollback()
-		logger.Error("mutation.QuitGroup: Committing transaction", rz.Err(err))
-		return ret, gqlerrors.New(groups.NewError(groups.ErrorQuittingGroup))
-	}
-
-	return ret, nil
+	ret = true
+	return
 }
--- cmd/bloom/server/domain/groups/quit_group.go
+++ cmd/bloom/server/domain/groups/quit_group.go
@@ -3,38 +3,70 @@ package groups
 import (
 	"context"
 
-	"github.com/jmoiron/sqlx"
-
+	"gitlab.com/bloom42/bloom/cmd/bloom/server/db"
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/domain/users"
 	"gitlab.com/bloom42/lily/rz"
+	"gitlab.com/bloom42/lily/uuid"
 )
 
-func QuitGroup(ctx context.Context, tx *sqlx.Tx, user users.User, group Group) error {
+func QuitGroup(ctx context.Context, actor *users.User, groupID uuid.UUID) (err error) {
 	logger := rz.FromCtx(ctx)
 	var remainingAdmins int
-	var err error
+	var group Group
 
-	if err = CheckUserIsGroupMember(ctx, tx, user.ID, group.ID); err != nil {
+	tx, err := db.DB.Beginx()
+	if err != nil {
+		logger.Error("groups.QuitGroup: Starting transaction", rz.Err(err))
+		err = NewError(ErrorQuittingGroup)
+		return
+	}
+
+	queryGetGroup := "SELECT * FROM groups WHERE id = $1"
+	err = tx.Get(&group, queryGetGroup, groupID)
+	if err != nil {
+		tx.Rollback()
+		logger.Error("groups.QuitGroup: fetching group", rz.Err(err),
+			rz.String("group.id", groupID.String()))
+		err = NewError(ErrorGroupNotFound)
+		return
+	}
+
+	if err = CheckUserIsGroupMember(ctx, tx, actor.ID, groupID); err != nil {
+		tx.Rollback()
 		return err
 	}
 
 	// delete membership
 	queryDeleteMembership := "DELETE FROM groups_members WHERE user_id = $1 AND group_id = $2"
-	_, err = tx.Exec(queryDeleteMembership, user.ID, group.ID)
+	_, err = tx.Exec(queryDeleteMembership, actor.ID, group.ID)
 	if err != nil {
-		logger.Error("groups.QuitGroup: removing members", rz.Err(err))
-		return NewError(ErrorQuittingGroup)
+		tx.Rollback()
+		logger.Error("groups.QuitGroup: removing member", rz.Err(err))
+		err = NewError(ErrorQuittingGroup)
+		return
 	}
 
 	queryRemainingAdmins := "SELECT COUNT(*) FROM groups_members WHERE group_id = $1 AND role = $2"
 	err = tx.Get(&remainingAdmins, queryRemainingAdmins, group.ID, RoleAdministrator)
 	if err != nil {
+		tx.Rollback()
 		logger.Error("groups.QuitGroup: error fetching remaining admins", rz.Err(err))
-		return NewError(ErrorQuittingGroup)
+		err = NewError(ErrorQuittingGroup)
+		return
 	}
 	if remainingAdmins != 0 {
-		return NewError(ErrorAtLeastOneAdministratorShouldRemainsInGroup)
+		tx.Rollback()
+		err = NewError(ErrorAtLeastOneAdministratorShouldRemainsInGroup)
+		return
 	}
 
-	return nil
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		logger.Error("groups.QuitGroup: Committing transaction", rz.Err(err))
+		err = NewError(ErrorQuittingGroup)
+		return
+	}
+
+	return
 }
