4b2a247a8fd9384da6029d70d48293ebf56adbe1
z0mbie42 <sylvain@kerkour.fr>
2020-04-27 11:21:45 +0200

    server: groups.RemoveMembers to domain

--- cmd/bloom/server/api/graphql/mutation/remove_group_members.go
+++ cmd/bloom/server/api/graphql/mutation/remove_group_members.go
@@ -6,48 +6,24 @@ import (
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/api/apiutil"
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/api/graphql/gqlerrors"
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/api/graphql/model"
-	"gitlab.com/bloom42/bloom/cmd/bloom/server/db"
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/domain/groups"
-	"gitlab.com/bloom42/lily/rz"
 )
 
 func (r *Resolver) RemoveGroupMembers(ctx context.Context, input model.RemoveGroupMembersInput) (*model.Group, error) {
 	var ret *model.Group
-	logger := rz.FromCtx(ctx)
 	currentUser := apiutil.UserFromCtx(ctx)
 
 	if currentUser == nil {
 		return ret, gqlerrors.AuthenticationRequired()
 	}
 
-	tx, err := db.DB.Beginx()
-	if err != nil {
-		logger.Error("mutation.RemoveGroupMembers: Starting transaction", rz.Err(err))
-		return ret, gqlerrors.New(groups.NewError(groups.ErrorRemovingMembersFromGroup))
+	params := groups.RemoveMembersParams{
+		GroupID:   input.ID,
+		Usernames: input.Members,
 	}
-
-	var group groups.Group
-
-	queryGetGroup := "SELECT * FROM groups WHERE id = $1"
-	err = tx.Get(&group, queryGetGroup, input.ID)
+	group, err := groups.RemoveMembers(ctx, currentUser, params)
 	if err != nil {
-		tx.Rollback()
-		logger.Error("groups.DeleteGroup: fetching group", rz.Err(err),
-			rz.String("group.id", input.ID.String()))
-		return ret, gqlerrors.New(groups.NewError(groups.ErrorGroupNotFound))
-	}
-
-	err = groups.RemoveMembers(ctx, tx, *currentUser, group, input.Members)
-	if err != nil {
-		tx.Rollback()
 		return ret, gqlerrors.New(err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		tx.Rollback()
-		logger.Error("mutation.RemoveGroupMembers: Committing transaction", rz.Err(err))
-		return ret, gqlerrors.New(groups.NewError(groups.ErrorRemovingMembersFromGroup))
 	}
 
 	ret = &model.Group{
--- cmd/bloom/server/domain/groups/remove_members.go
+++ cmd/bloom/server/domain/groups/remove_members.go
@@ -5,41 +5,81 @@ import (
 
 	"github.com/jmoiron/sqlx"
 
+	"gitlab.com/bloom42/bloom/cmd/bloom/server/db"
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/domain/users"
 	"gitlab.com/bloom42/lily/rz"
+	"gitlab.com/bloom42/lily/uuid"
 )
 
-func RemoveMembers(ctx context.Context, tx *sqlx.Tx, user users.User, group Group, usernames []string) error {
-	logger := rz.FromCtx(ctx)
-	var err error
-	var remainingAdmins int
+type RemoveMembersParams struct {
+	GroupID   uuid.UUID
+	Usernames []string
+}
 
-	if err = CheckUserIsGroupAdmin(ctx, tx, user.ID, group.ID); err != nil {
-		return err
+func RemoveMembers(ctx context.Context, actor *users.User, params RemoveMembersParams) (ret *Group, err error) {
+	logger := rz.FromCtx(ctx)
+	var remainingAdmins int
+	var group Group
+
+	tx, err := db.DB.Beginx()
+	if err != nil {
+		logger.Error("groups.RemoveMembers: Starting transaction", rz.Err(err))
+		err = NewError(ErrorRemovingMembersFromGroup)
+		return
+	}
+
+	err = CheckUserIsGroupAdmin(ctx, tx, actor.ID, group.ID)
+	if err != nil {
+		tx.Rollback()
+		return
+	}
+
+	queryGetGroup := "SELECT * FROM groups WHERE id = $1"
+	err = tx.Get(&group, queryGetGroup, params.GroupID)
+	if err != nil {
+		tx.Rollback()
+		logger.Error("groups.RemoveMembers: fetching group", rz.Err(err),
+			rz.String("group.id", params.GroupID.String()))
+		err = NewError(ErrorGroupNotFound)
+		return
 	}
 
 	// delete memberships
 	queryStr := `DELETE FROM groups_members
 		INNER JOIN users ON users.id = groups_members.user_id
 		WHERE users.username IN ($1)`
-	query, args, err := sqlx.In(queryStr, usernames)
+	query, args, err := sqlx.In(queryStr, params.Usernames)
 	query = tx.Rebind(query)
 	_, err = tx.Exec(query, args...)
 	if err != nil {
-		logger.Error("removing members", rz.Err(err))
-		return NewError(ErrorRemovingMembersFromGroup)
+		tx.Rollback()
+		logger.Error("groups.RemoveMembers: removing members", rz.Err(err))
+		err = NewError(ErrorRemovingMembersFromGroup)
+		return
 	}
 
 	queryRemainingAdmins := `SELECT COUNT(*) FROM groups_members
 		WHERE group_id = $1 AND role = $2`
 	err = tx.Get(&remainingAdmins, queryRemainingAdmins, group.ID, RoleAdministrator)
 	if err != nil {
-		logger.Error("error fetching remaining admins", rz.Err(err))
-		return NewError(ErrorRemovingMembersFromGroup)
+		tx.Rollback()
+		logger.Error("groups.RemoveMembers: error fetching remaining admins", rz.Err(err))
+		err = NewError(ErrorRemovingMembersFromGroup)
+		return
 	}
 	if remainingAdmins != 0 {
-		return NewError(ErrorAtLeastOneAdministratorShouldRemainsInGroup)
+		tx.Rollback()
+		err = NewError(ErrorAtLeastOneAdministratorShouldRemainsInGroup)
+		return
 	}
 
-	return nil
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		logger.Error("groups.RemoveMembers: Committing transaction", rz.Err(err))
+		err = NewError(ErrorRemovingMembersFromGroup)
+	}
+
+	ret = &group
+	return
 }
