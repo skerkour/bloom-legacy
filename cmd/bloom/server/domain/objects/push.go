45a7e6829c14a5b8fd5f368a665abd7f5c9379b9
z0mbie42 <sylvain@kerkour.fr>
2020-04-28 17:13:26 +0200

    server: start to implement push

--- cmd/bloom/server/domain/objects/push.go
+++ cmd/bloom/server/domain/objects/push.go
@@ -4,9 +4,31 @@ import (
 	"context"
 
 	"gitlab.com/bloom42/bloom/cmd/bloom/server/domain/users"
+	"gitlab.com/bloom42/lily/uuid"
 )
 
+type PushParams struct {
+	Me     *RepositoryPush
+	Groups []RepositoryPush
+}
+
+type RepositoryPush struct {
+	CurrentState string
+	Objects      []APIObject
+	GroupID      *uuid.UUID
+}
+
+type PushResult struct {
+	Me     *RepositoryPushResult
+	Groups []RepositoryPushResult
+}
+
+type RepositoryPushResult struct {
+	NewState string
+	GroupID  *uuid.UUID
+}
+
 // Push is used to push changes
-func Push(ctx context.Context, actor *users.User) (err error) {
+func Push(ctx context.Context, actor *users.User, params PushParams) (err error) {
 	return
 }
