b8d2f7b38ac9de7cd6c699276cdbafb97aa47196
z0mbie42 <sylvain@kerkour.fr>
2020-05-14 15:20:09 +0200

    core/objects: Add 'Conflict' to conflicting objects

--- core/domain/objects/dedup_object.go
+++ core/domain/objects/dedup_object.go
@@ -1,5 +1,9 @@
 package objects
 
+import (
+	"encoding/json"
+)
+
 func dedupObject(object *Object, username []byte) (*Object, error) {
 	var err error
 	ret := &Object{}
@@ -10,5 +14,40 @@ func dedupObject(object *Object, username []byte) (*Object, error) {
 	ret.GroupID = object.GroupID
 	ret.OutOfSync = true
 	ret.ID, err = GenerateObjectID(username)
+	dedupObjectAddConflict(ret)
 	return ret, err
+}
+
+func dedupObjectAddConflict(object *Object) {
+	objDataMap := map[string]interface{}{}
+	var err error
+
+	err = json.Unmarshal(object.Data, &objDataMap)
+	if err != nil {
+		return
+	}
+	switch object.Type {
+	case "com.bloom42.bloom.note":
+		if val, ok := objDataMap["title"]; ok {
+			if valStr, ok := val.(string); ok {
+				objDataMap["title"] = "[Conflict] " + valStr
+			}
+		}
+	case "com.bloom42.bloom.contact":
+		if val, ok := objDataMap["firstName"]; ok {
+			if valStr, ok := val.(string); ok {
+				objDataMap["firstName"] = "[Conflict] " + valStr
+			}
+		}
+	case "com.bloom42.bloom.calendar_event":
+		if val, ok := objDataMap["title"]; ok {
+			if valStr, ok := val.(string); ok {
+				objDataMap["title"] = "[Conflict] " + valStr
+			}
+		}
+	}
+	jsonData, err := json.Marshal(objDataMap)
+	if err != nil {
+		object.Data = jsonData
+	}
 }
