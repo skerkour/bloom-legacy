348df956e8932f0fdd82d04796aa480048ef4687
z0mbie42 <sylvain@kerkour.fr>
2020-05-14 14:34:02 +0200

    core: Add comments

--- core/domain/users/const.go
+++ core/domain/users/const.go
@@ -8,6 +8,11 @@ const (
 	MASTER_KEY_KEY  = "master_key"
 	PUBLIC_KEY_KEY  = "public_key"
 	PRIVATE_KEY_KEY = "private_key"
+
+	// ME_KEY is the preferences key used to save me
+	ME_KEY = "me"
+	// SESSION_KEY is the preferences key used to save the session
+	SESSION_KEY = "session"
 )
 
 var PASSWORD_KDF_PARAMS = crypto.DefaultDeriveKeyFromPasswordParams
--- core/domain/users/save_session.go
+++ core/domain/users/save_session.go
@@ -10,6 +10,7 @@ import (
 	"gitlab.com/bloom42/bloom/core/domain/preferences"
 )
 
+// SaveSignedIn saves signedIn info in preferences
 func SaveSignedIn(signin *model.SignedIn) error {
 	ctx := context.Background()
 
@@ -39,22 +40,24 @@ func SaveSignedIn(signin *model.SignedIn) error {
 	return nil
 }
 
+// SaveMe saves me in preferences
 func SaveMe(ctx context.Context, tx *sqlx.Tx, me *model.User) error {
 	meData, err := json.Marshal(me)
 	if err != nil {
 		return err
 	}
 
-	err = preferences.Set(ctx, tx, "me", string(meData))
+	err = preferences.Set(ctx, tx, ME_KEY, string(meData))
 	return err
 }
 
+// SaveSession saves session in preferences
 func SaveSession(ctx context.Context, tx *sqlx.Tx, session *model.Session) error {
 	sessionData, err := json.Marshal(session)
 	if err != nil {
 		return err
 	}
 
-	err = preferences.Set(ctx, tx, "session", string(sessionData))
+	err = preferences.Set(ctx, tx, SESSION_KEY, string(sessionData))
 	return err
 }
