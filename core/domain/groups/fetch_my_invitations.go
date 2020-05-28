d110ca2a4c61ce7b0e83a52041d8acdcbb99d493
Sylvain Kerkour <sylvain@kerkour.fr>
2020-05-28 17:02:41 +0200

    core/groups: FetchMyInvitations

---
+++ core/domain/groups/fetch_my_invitations.go
@@ -0,0 +1,44 @@
+package groups
+
+import (
+	"context"
+
+	"gitlab.com/bloom42/bloom/core/api"
+	"gitlab.com/bloom42/bloom/core/api/model"
+	"gitlab.com/bloom42/gobox/graphql"
+)
+
+func FetchMyGroupInvitations() (*model.User, error) {
+	client := api.Client()
+
+	var resp struct {
+		Me *model.User `json:"me"`
+	}
+	req := graphql.NewRequest(`
+	query {
+		me {
+			invitations {
+				nodes {
+					group {
+						name
+						description
+					}
+					inviter {
+						username
+						displayName
+					}
+					ephemeralPublicKey
+					signature
+					encryptedMasterKey
+					encryptedMasterKeySignature
+				}
+				totalCount
+			}
+		}
+	}
+	`)
+
+	err := client.Do(context.Background(), req, &resp)
+
+	return resp.Me, err
+}
