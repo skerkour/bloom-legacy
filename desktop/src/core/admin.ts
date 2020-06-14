acde8c37c22f8f62b91e60097b580d815e0326eb
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-14 22:44:18 +0200

    desktop

--- desktop/src/core/admin.ts
+++ desktop/src/core/admin.ts
@@ -1,6 +1,5 @@
 import { Maybe, BloomMetadata, UserConnection } from '@/api/models';
 
-/* eslint-disable import/prefer-default-export */
 export enum Method {
   FetchDashBoardData = 'admin.fetchDashboardData',
 }
