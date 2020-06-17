8dd4643947e367d7cfbf06d3205a4b2c60d159a1
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-17 17:51:46 +0200

    server/billing: refactor FindPaymentMethodsForUser

--- server/domain/billing/service/find_payment_methods_for_user.go
+++ server/domain/billing/service/find_payment_methods_for_user.go
@@ -4,23 +4,21 @@ import (
 	"context"
 
 	"gitlab.com/bloom42/bloom/server/domain/billing"
+	"gitlab.com/bloom42/bloom/server/domain/users"
 	"gitlab.com/bloom42/gobox/uuid"
 )
 
 func (service *BillingService) FindPaymentMethodsForUser(ctx context.Context, userID uuid.UUID) (ret []billing.PaymentMethod, err error) {
+	me, err := service.usersService.Me(ctx)
+	if err != nil {
+		return
+	}
+
+	if me.ID != userID && !me.IsAdmin {
+		err = users.ErrPermissionDenied
+		return
+	}
+
+	ret, err = service.billingRepo.FindPaymentMethodsForUser(ctx, service.db, userID)
 	return
 }
-
-/*
-	var ret *PaymentMethodConnection
-	currentUser := apiutil.UserFromCtx(ctx)
-
-	if currentUser.ID != uuid.UUID(*user.ID) && !currentUser.IsAdmin {
-		return ret, gqlerrors.AdminRoleRequired()
-	}
-
-	paymentMethods, err := billing.FindPaymentMethodsByUserId(ctx, nil, uuid.UUID(*user.ID).String())
-	if err != nil {
-		return ret, gqlerrors.New(err)
-	}
-*/
