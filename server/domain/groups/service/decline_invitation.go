da226728249dd8ef0b2db575f20969e139b18d98
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-18 12:03:49 +0200

    server/groups: refactor service.DeclineInvitation

--- server/domain/groups/service/decline_invitation.go
+++ server/domain/groups/service/decline_invitation.go
@@ -3,9 +3,27 @@ package service
 import (
 	"context"
 
+	"gitlab.com/bloom42/bloom/server/domain/groups"
 	"gitlab.com/bloom42/gobox/uuid"
 )
 
 func (service *GroupsService) DeclineInvitation(ctx context.Context, invitationID uuid.UUID) (err error) {
+	me, err := service.usersService.Me(ctx)
+	if err != nil {
+		return
+	}
+
+	invitation, err := service.groupsRepo.FindInvitationByID(ctx, service.db, invitationID)
+	if err != nil {
+		return
+	}
+
+	// validate action
+	if actor.ID != invitation.InviteeID {
+		err = groups.ErrInvitationNotFound
+		return
+	}
+
+	err = service.groupsRepo.DeleteInvitation(ctx, service.db, invitationID)
 	return
 }
