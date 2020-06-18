c75740dc05685a6d9f4c1c53f1a9d13b6d9e657b
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-18 18:22:24 +0200

    server/users: service.VerifySessionToken

--- server/domain/users/service/verify_session_token.go
+++ server/domain/users/service/verify_session_token.go
@@ -4,8 +4,36 @@ import (
 	"context"
 
 	"gitlab.com/bloom42/bloom/server/domain/users"
+	"gitlab.com/bloom42/bloom/server/errors"
 )
 
 func (service *UsersService) VerifySessionToken(ctx context.Context, token string) (user users.User, session users.Session, err error) {
+	sessionID, sessionSecret, err := decodeSessionToken(ctx, token)
+	if err != nil {
+		return
+	}
+
+	session, err = service.userRepo.FindSessionByID(ctx, service.db, sessionID)
+	if err != nil {
+		if _, ok := err.(*errors.NotFoundError); ok {
+			err = users.ErrInvalidSession
+			return
+		}
+		return
+	}
+
+	err = verifySessionSecret(ctx, retSession, sessionSecret)
+	if err != nil {
+		return
+	}
+
+	user, err = service.userRepo.FindUserByID(ctx, service.db, retSession.UserID)
+	if err != nil {
+		if _, ok := err.(*errors.NotFoundError); ok {
+			err = users.ErrInvalidSession
+			return
+		}
+		return
+	}
 	return
 }
