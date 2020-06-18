c20be86747bdb30f8fba30318f773f7781b8c277
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-18 10:27:38 +0200

    server/billing: refactor repository.FindCustomerByUserID

--- server/domain/billing/repository/find_customer_by_user_id.go
+++ server/domain/billing/repository/find_customer_by_user_id.go
@@ -2,12 +2,28 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 
 	"gitlab.com/bloom42/bloom/server/db"
 	"gitlab.com/bloom42/bloom/server/domain/billing"
+	"gitlab.com/bloom42/bloom/server/errors"
+	"gitlab.com/bloom42/gobox/log"
 	"gitlab.com/bloom42/gobox/uuid"
 )
 
 func (repo *BillingRepository) FindCustomerByUserID(ctx context.Context, db db.Queryer, userID uuid.UUID) (ret billing.Customer, err error) {
+	query := "SELECT * FROM billing_customers WHERE user_id = $1"
+
+	err = db.Get(ctx, &ret, query, userID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			err = errors.NotFound("Customer not found")
+		} else {
+			logger := log.FromCtx(ctx)
+			const errMessage = "billing.FindCustomerByUserID: finding customer"
+			logger.Error(errMessage, log.Err("error", err), log.UUID("user.id", userID))
+			err = errors.Internal(errMessage, err)
+		}
+	}
 	return
 }
