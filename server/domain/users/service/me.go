a2881c6f4473f05ec3960e16607910bc5dac3ce4
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-18 17:51:30 +0200

    server/users: service.Me

--- server/domain/users/service/me.go
+++ server/domain/users/service/me.go
@@ -4,8 +4,17 @@ import (
 	"context"
 
 	"gitlab.com/bloom42/bloom/server/domain/users"
+	"gitlab.com/bloom42/bloom/server/http/httputil"
 )
 
 func (service *UsersService) Me(ctx context.Context) (me users.User, err error) {
+	httpCtx := httputil.HTTPCtxFromCtx(ctx)
+
+	if httpCtx.AuthenticatedUser == nil {
+		err = users.ErrAuthenticationRequired
+		return
+	}
+
+	ret = *httpCtx.AuthenticatedUser
 	return
 }
