19775f46fa40263503e4c63064853c30d1fb7c90
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-18 10:05:17 +0200

    server/billing: refactor repository.CreateInvoice

--- server/domain/billing/repository/create_invoice.go
+++ server/domain/billing/repository/create_invoice.go
@@ -5,23 +5,22 @@ import (
 
 	"gitlab.com/bloom42/bloom/server/db"
 	"gitlab.com/bloom42/bloom/server/domain/billing"
+	"gitlab.com/bloom42/bloom/server/errors"
+	"gitlab.com/bloom42/gobox/log"
 )
 
 func (repo *BillingRepository) CreateInvoice(ctx context.Context, db db.Queryer, invoice billing.Invoice) (err error) {
-	return
-}
-
-/*
-
-	queryCreate := `INSERT INTO billing_invoices
+	query := `INSERT INTO billing_invoices
 	(id, created_at, updated_at, paid_at, amount, stripe_id, stripe_hosted_url, stripe_pdf_url, customer_id)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
-	_, err = tx.Exec(queryCreate, ret.ID, ret.CreatedAt, ret.UpdatedAt, ret.PaidAt, ret.Amount, ret.StripeID,
-		ret.StripeHostedURL, ret.StripePdfURL, ret.CustomerID)
-	if err != nil {
-		tx.Rollback()
-		logger.Error("billing.CreateInvoice: inserting new invoice", rz.Err(err))
-		return ret, NewError(ErrorCreatingInvoice)
-	}
 
-*/
+	_, err = db.Exec(ctx, query, invoice.ID, invoice.CreatedAt, invoice.UpdatedAt, invoice.PaidAt,
+		invoice.Amount, invoice.StripeID, invoice.StripeHostedURL, invoice.StripePdfURL, invoice.CustomerID)
+	if err != nil {
+		logger := log.FromCtx(ctx)
+		const errMessage = "billing.CreateInvoice: inserting invoice"
+		logger.Error(errMessage, log.Err("error", err), log.UUID("invoice.id", invoice.ID))
+		err = errors.Internal(errMessage, err)
+	}
+	return
+}
