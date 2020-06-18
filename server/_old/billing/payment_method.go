16df731f289357f67ead00fb8dc1f21741b84534
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-18 10:31:01 +0200

    server/billing: refactor repository.FindDefaultPaymentMethodForCustomer

--- server/_old/billing/payment_method.go
+++ server/_old/billing/payment_method.go
@@ -25,21 +25,3 @@ func FindPaymentMethodById(ctx context.Context, tx *sqlx.Tx, id uuid.UUID) (*Pay
 	ret = &paymentMethod
 	return ret, nil
 }
-
-func FindPaymentMethodByCustomer(ctx context.Context, tx *sqlx.Tx, customer *Customer, isDefault bool) (*PaymentMethod, error) {
-	var ret *PaymentMethod
-	var paymentMethod PaymentMethod
-	var err error
-	logger := rz.FromCtx(ctx)
-
-	queryFind := "SELECT * FROM billing_payment_methods WHERE customer_id = $1 AND is_default = $2"
-	err = tx.Get(&paymentMethod, queryFind, customer.ID, isDefault)
-	if err != nil {
-		logger.Error("billing.FindPaymentMethodByCustomer: finding payment method", rz.Err(err),
-			rz.String("customer.id", customer.ID.String()))
-		return ret, NewError(ErrorPaymentMethodNotFound)
-	}
-
-	ret = &paymentMethod
-	return ret, nil
-}
