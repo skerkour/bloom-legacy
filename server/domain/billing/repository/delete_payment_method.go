9c639be9de1be591e0291ed40612d75094df3b46
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-18 10:13:12 +0200

    server/billing: refactor repository.DeletePaymentMethod

--- server/domain/billing/repository/delete_payment_method.go
+++ server/domain/billing/repository/delete_payment_method.go
@@ -4,9 +4,20 @@ import (
 	"context"
 
 	"gitlab.com/bloom42/bloom/server/db"
+	"gitlab.com/bloom42/bloom/server/errors"
+	"gitlab.com/bloom42/gobox/log"
 	"gitlab.com/bloom42/gobox/uuid"
 )
 
 func (repo *BillingRepository) DeletePaymentMethod(ctx context.Context, db db.Queryer, paymentMethodID uuid.UUID) (err error) {
+	query := "DELETE FROM billing_payment_methods WHERE id = $1"
+
+	_, err = db.Exec(ctx, query, paymentMethodID)
+	if err != nil {
+		logger := log.FromCtx(ctx)
+		const errMessage = "billing.DeletePaymentMethod: deleting payment_method"
+		logger.Error(errMessage, log.Err("error", err), log.UUID("payment_method.id", paymentMethodID))
+		err = errors.Internal(errMessage, err)
+	}
 	return
 }
