76aafb2aa06c2691eeea3fd4e26b67fc31355555
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-18 10:07:46 +0200

    server/billing: refactor repository.CreatePlan

--- server/domain/billing/repository/create_plan.go
+++ server/domain/billing/repository/create_plan.go
@@ -5,25 +5,22 @@ import (
 
 	"gitlab.com/bloom42/bloom/server/db"
 	"gitlab.com/bloom42/bloom/server/domain/billing"
+	"gitlab.com/bloom42/bloom/server/errors"
+	"gitlab.com/bloom42/gobox/log"
 )
 
 func (repo *BillingRepository) CreatePlan(ctx context.Context, db db.Queryer, plan billing.Plan) (err error) {
+	query := `INSERT INTO billing_plans
+	(id, created_at, updated_at, name, description, stripe_id, price, product, storage)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+	_, err = db.Exec(ctx, query, plan.ID, plan.CreatedAt, plan.UpdatedAt, plan.Name, plan.Description,
+		plan.StripeID, plan.Price, plan.Product, plan.Storage)
+	if err != nil {
+		logger := log.FromCtx(ctx)
+		const errMessage = "billing.CreatePlan: inserting plan"
+		logger.Error(errMessage, log.Err("error", err), log.UUID("plan.id", plan.ID))
+		err = errors.Internal(errMessage, err)
+	}
 	return
 }
-
-/*
-
-	// create plan
-	queryCreatePlan := `INSERT INTO billing_plans
-		(id, created_at, updated_at, name, description, stripe_id, price, is_public, product, storage)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
-	_, err = tx.Exec(queryCreatePlan, ret.ID, ret.CreatedAt, ret.UpdatedAt, ret.Name, ret.Description,
-		ret.StripeID, ret.Price, ret.IsPublic, ret.Product, ret.Storage)
-	if err != nil {
-		tx.Rollback()
-		logger.Error("billing.CreatePlan: inserting new plan", rz.Err(err))
-		err = NewError(ErrorCreatingPlan)
-		return
-	}
-
-*/
